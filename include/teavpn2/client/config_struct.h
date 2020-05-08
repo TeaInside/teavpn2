0efa26607b0b5260e6b9a908fb91b643a431f459
Ammar Faizi <ammarfaizi2@gmail.com>
2020-05-08 16:12:37 +0700

    Prepare config struct

---
+++ include/teavpn2/client/config_struct.h
@@ -0,0 +1,6 @@
+
+#ifndef TEAVPN__CLIENT__CONFIG_STRUCT_H
+#define TEAVPN__CLIENT__CONFIG_STRUCT_H
+
+
+#endif
---
+++ include/teavpn2/server/config_struct.h
@@ -0,0 +1,6 @@
+
+#ifndef TEAVPN__SERVER__CONFIG_STRUCT_H
+#define TEAVPN__SERVER__CONFIG_STRUCT_H
+
+
+#endif
