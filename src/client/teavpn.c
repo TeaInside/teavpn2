eb3f8fcd75712ea52564ed8767baeb131b0b7ed1
Ammar Faizi <ammarfaizi2@gmail.com>
2020-05-10 20:59:49 +0700

    Clean up

--- src/client/teavpn.c
+++ src/client/teavpn.c
@@ -1,7 +1,68 @@
 
 #include <teavpn2/client/common.h>
 
+/**
+ * @param teavpn_server_config *config
+ * @return int
+ */
 int teavpn_client_run(teavpn_client_config *config)
 {
+if (!validate_config(config)) {
+    return 1;
+  }
 
+  int ret = 1;
+  iface_info iinfo;
+
+  debug_log(2, "Allocating teavpn interface...");
+  iinfo.tun_fd = teavpn_iface_allocate(config->iface.dev);
+  if (iinfo.tun_fd < 0) {
+    return 1; /* No need to close tun_fd, since failed to create. */
+  }
+
+  debug_log(2, "Setting up teavpn network interface...");
+  if (!teavpn_iface_init(&config->iface)) {
+    error_log("Cannot set up teavpn network interface");
+    ret = 1;
+    goto close;
+  }
+
+  switch (config->socket_type) {
+    case teavpn_sock_tcp:
+      ret = teavpn_tcp_run(&iinfo, config);
+      break;
+
+    case teavpn_sock_udp:
+      /* TODO: Make VPN be able to use UDP socket. */
+      break;
+
+    default:
+      error_log("Invalid socket type");
+      return 1;
+      break;
+  }
+
+close:
+  /* Close tun_fd. */
+  close(iinfo.tun_fd);
+  return ret;
+}
+
+
+/**
+ * @param teavpn_server_config *config
+ * @return bool
+ */
+static bool validate_config(teavpn_server_config *config)
+{
+  /**
+   * Check data dir.
+   */
+  debug_log(5, "Checking data_dir...");
+  if (config->data_dir == NULL) {
+    error_log("Data dir cannot be empty!");
+    return false;
+  }
+
+  return true;
 }
