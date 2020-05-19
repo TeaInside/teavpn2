dc244a83d8d130fe6e533f02ea34a6363fba25d5
Ammar Faizi <ammarfaizi2@gmail.com>
2020-05-19 21:55:37 +0700

    Update main.tcp

--- src/server/socket/tcp/main_tcp.c
+++ src/server/socket/tcp/main_tcp.c
@@ -610,7 +610,7 @@ inline static void teavpn_server_tcp_handle_client_data(tcp_channel *chan)
     register struct iphdr *hdr = (struct iphdr *)cli_pkt->data;
     register tcp_channel *channels = chan->mstate->channels;
 
-    register unsigned char *pt = (unsigned char *)cli_pkt->data[i];
+    register unsigned char *pt = (unsigned char *)cli_pkt->data;
     for (uint16_t i = 0; i < cli_pkt->len; i++) {
       printf("%02x ", pt[i]);
     }
