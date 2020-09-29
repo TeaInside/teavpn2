5ddbf1e5f4d35abfb54123fe3bd3807ca5eb1e49
Ammar Faizi <ammarfaizi2@gmail.com>
2020-09-29 18:30:13 +0700

    [memory] Prepare arena tr allocator
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/include/teavpn2/global/common.h
+++ src/include/teavpn2/global/common.h
@@ -3,10 +3,17 @@
 #define TEAVPN2__GLOBAL__COMMON_H
 
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 typedef enum {
   sock_tcp,
   sock_udp
 } socket_type;
+
+void t_ar_init(register void *ptr, register size_t len);
+void *t_ar_alloc(register size_t len);
+char *t_ar_strdup(register const char *str);
+char *t_ar_strndup(register const char *str, register size_t tlen);
 
 #endif
--- src/sources/teavpn2/server/config.c
+++ src/sources/teavpn2/server/config.c
@@ -1,7 +1,7 @@
 
 #include <teavpn2/server/common.h>
 
-void tvpn_server_load_config_file(char *file, server_cfg *config)
+bool tvpn_server_load_config_file(char *file, server_cfg *config)
 {
 
 }
