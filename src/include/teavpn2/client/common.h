2f3907e1a9d0e9420c4de110f14669269860ece7
Ammar Faizi <ammarfaizi2@gmail.com>
2020-10-02 16:14:03 +0700

    [client] Prepare common header
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/include/teavpn2/client/common.h
+++ src/include/teavpn2/client/common.h
@@ -1,7 +1,47 @@
 
-#ifndef TEAVPN2__CLIENT__COMMON_H
-#define TEAVPN2__CLIENT__COMMON_H
+#ifndef TEAVPN2__SERVER__COMMON_H
+#define TEAVPN2__SERVER__COMMON_H
 
 #include <teavpn2/global/common.h>
+
+typedef struct _client_iface_cfg {
+  char                  *dev;           /* Interface name. */
+  uint16_t              mtu;            /* MTU. */
+} client_iface_cfg;
+
+
+
+typedef struct _client_socket_cfg {
+  char                  *server_addr    /* Socket client bind address. */
+  int                   backlog;        /* Socket listen backlog. */
+  uint16_t              server_port;    /* Socket client bind port. */
+  socket_type           type;           /* Socket type, TCP/UDP. */
+} client_socket_cfg;
+
+
+typedef struct _client_cfg {
+
+  char                  *config_file;   /* Config file. */
+  char                  *data_dir;      /* Data directory. */
+  client_iface_cfg      iface;          /* Virtual interface configuration. */
+  socket_cfg            sock;           /* Socket configuration. */
+
+} client_cfg;
+
+
+
+/* argv_parser */
+bool tvpn_client_argv_parse(
+  int argc,
+  char *argv[],
+  char *envp[],
+  client_cfg *config
+);
+/* End of argv_parser */
+
+
+/* config */
+bool tvpn_client_load_config_file(char *file, client_cfg *config);
+/* End of config */
 
 #endif
