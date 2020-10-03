13439f2f390333b7dccffa48cabe3ca49e9bcdb8
Ammar Faizi <ammarfaizi2@gmail.com>
2020-10-03 21:54:42 +0700

    [client] Prepare tun interface initialization
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/sources/teavpn2/client/sock/tcp/client.c
+++ src/sources/teavpn2/client/sock/tcp/client.c
@@ -22,9 +22,9 @@
 inline static void tvpn_client_tcp_signal_handler(int signal);
 inline static bool tvpn_client_tcp_iface_init(client_tcp_state * __restrict__ state);
 inline static bool tvpn_client_tcp_sock_init(client_tcp_state * __restrict__ state);
-inline static bool tvpn_client_tcp_sock_init(client_tcp_state * __restrict__ state);
-inline static void tvpn_client_tcp_accept(client_tcp_state * __restrict__ state);
 inline static bool tvpn_client_tcp_socket_setup(int fd);
+
+static client_tcp_state *g_state = NULL;
 
 /**
  * @param client_cfg *config
@@ -33,13 +33,35 @@ inline static bool tvpn_client_tcp_socket_setup(int fd);
 __attribute__((force_align_arg_pointer))
 int tvpn_client_tcp_run(client_cfg *config)
 {
-  int ret = 1;
+  int                   ret = 1;
+  int                   pipe_fd[2] = {-1, -1};
+  client_tcp_state      state;
+  struct pollfd         fds[2];
+  nfds_t                nfds;
+  int                   ptimeout;
 
 
+  state.net_fd = -1;
+  state.stop   = false;
+  g_state      = &state;
+  state.config = config;
 
-
+  debug_log(2, "Allocating virtual network interface...");
+  if (!tvpn_client_tcp_iface_init(&state)) {
+    goto ret;
+  }
 
   ret:
 
   return ret;
+}
+
+
+/**
+ * @param  client_tcp_state * __restrict__ state
+ * @return bool
+ */
+inline static bool tvpn_client_tcp_iface_init(client_tcp_state * __restrict__ state)
+{
+
 }
