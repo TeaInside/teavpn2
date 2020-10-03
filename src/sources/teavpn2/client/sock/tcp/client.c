19aa464c988673efca8210f09c5486854592290f
Ammar Faizi <ammarfaizi2@gmail.com>
2020-10-03 21:38:20 +0700

    [client] Prepare client config validator
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ src/sources/teavpn2/client/sock/tcp/client.c
@@ -0,0 +1,18 @@
+
+#include <poll.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <linux/if.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/tcp.h>
+#include <linux/if_tun.h>
+
+#include <teavpn2/server/common.h>
