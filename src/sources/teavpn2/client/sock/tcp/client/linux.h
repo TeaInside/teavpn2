eb939c0230345517755527d0a3841aca0095ebc7
Ammar Faizi <ammarfaizi2@gmail.com>
2020-10-10 22:55:21 +0700

    [client] Add client/linux.h for TCP prot
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/sources/teavpn2/client/sock/tcp/client/linux.h
+++ src/sources/teavpn2/client/sock/tcp/client/linux.h
@@ -1,8 +1,14 @@
 
-#include <teavpn2/client/common.h>
-
-#if defined(__linux__)
-#  include "client/linux.h"
-#else
+#if !defined(__linux__)
 #  error "Compiler is not supported!"
 #endif
+
+#ifndef TEAVPN2__CLIENT__SOCK__TCP__CLIENT__LINUX_H
+#define TEAVPN2__CLIENT__SOCK__TCP__CLIENT__LINUX_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#endif /* #ifndef TEAVPN2__CLIENT__SOCK__TCP__CLIENT__LINUX_H */
