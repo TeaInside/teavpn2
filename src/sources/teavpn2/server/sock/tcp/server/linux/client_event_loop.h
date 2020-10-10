6b54f6b0d786709489bab6550f9d47d736f3c201
Ammar Faizi <ammarfaizi2@gmail.com>
2020-10-10 18:33:41 +0700

    [server] Update server event loop
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/sources/teavpn2/server/sock/tcp/server/linux/client_event_loop.h
+++ src/sources/teavpn2/server/sock/tcp/server/linux/client_event_loop.h
@@ -7,5 +7,17 @@
 #ifndef TEAVPN2__SERVER__SOCK__TCP__SERVER__LINUX__CLIENT_EVENT_LOOP_H
 #define TEAVPN2__SERVER__SOCK__TCP__SERVER__LINUX__CLIENT_EVENT_LOOP_H
 
+inline static void *
+tvpn_server_tcp_thread_worker(void *__restrict__ _chan)
+{
+  tcp_channel *chan = (tcp_channel *)_chan;
+
+
+  debug_log(0, "[%s:%d] Spawning a thread to handle new connection",
+            HP_CC(chan));
+
+  return NULL; 
+}
+
 #endif /* #ifndef
 TEAVPN2__SERVER__SOCK__TCP__SERVER__LINUX__CLIENT_EVENT_LOOP_H */
