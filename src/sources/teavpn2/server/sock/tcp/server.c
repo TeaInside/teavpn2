5e636a22a577e982f5e7ac14e250178494d3d48f
Ammar Faizi <ammarfaizi2@gmail.com>
2020-10-21 20:31:05 +0700

    Refactoring makefile and project directory to isolate modules
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/sources/teavpn2/server/sock/tcp/server.c
+++ src/sources/teavpn2/server/sock/tcp/server.c
@@ -2,42 +2,44 @@
 #include <teavpn2/server/common.h>
 #include <teavpn2/global/packet.h>
 
-#define RACT(EXPR) ((state.stop) || (EXPR))
+// #define RACT(EXPR) ((state.stop) || (EXPR))
 
-server_tcp_state *g_state;
+// server_tcp_state *g_state;
 
-#if defined(__linux__)
-#  include "server/linux.h"
-#else
-#  error "Compiler is not supported!"
-#endif
+// #if defined(__linux__)
+// #  include "server/linux.h"
+// #else
+// #  error "Compiler is not supported!"
+// #endif
 
-/**
- * @param server_cfg *config
- * @return int
- */
+// /**
+//  * @param server_cfg *config
+//  * @return int
+//  */
 int
 tvpn_server_tcp_run(server_cfg *config)
 {
-  int               ret    = 1; /* Exit code. */
-  server_tcp_state  state;
+//   int               ret    = 1; /* Exit code. */
+//   server_tcp_state  state;
 
-  g_state = &state;
+//   g_state = &state;
 
+//   if (RACT(!tvpn_server_tcp_init_iface(&state))) {
+//     goto ret;
+//   }
 
-  if (RACT(!tvpn_server_tcp_init_iface(&state))) {
-    goto ret;
-  }
+//   if (RACT(!tvpn_server_tcp_init_pipe(state.pipe_fd))) {
+//     goto ret;
+//   }
 
-  if (RACT(!tvpn_server_tcp_init_pipe(state.pipe_fd))) {
-    goto ret;
-  }
+//   if (RACT(!tvpn_server_tcp_init_socket(&state))) {
+//     goto ret;
+//   }
 
-  if (RACT(!tvpn_server_tcp_init_socket(&state))) {
-    goto ret;
-  }
+// ret:
+//   tvpn_server_tcp_clean_up(&state);
+//   return ret;
 
-ret:
-  tvpn_server_tcp_clean_up(&state);
-  return ret;
+  (void)config;
+  return 0;
 }
