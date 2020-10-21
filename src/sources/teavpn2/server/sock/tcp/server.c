8b00a2b480c28615a4a72bd69ab4dec8c0b44605
Ammar Faizi <ammarfaizi2@gmail.com>
2020-10-21 16:28:30 +0700

    [server][linux] Prepare client handler event loop
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/sources/teavpn2/server/sock/tcp/server.c
+++ src/sources/teavpn2/server/sock/tcp/server.c
@@ -2,6 +2,10 @@
 #include <teavpn2/server/common.h>
 #include <teavpn2/global/packet.h>
 
+#define RACT(EXPR) ((state.stop) || (EXPR))
+
+server_tcp_state *g_state;
+
 #if defined(__linux__)
 #  include "server/linux.h"
 #else
@@ -15,5 +19,25 @@
 int
 tvpn_server_tcp_run(server_cfg *config)
 {
-  return __internal_tvpn_server_tcp_run(config);
+  int               ret    = 1; /* Exit code. */
+  server_tcp_state  state;
+
+  g_state = &state;
+
+
+  if (RACT(!tvpn_server_tcp_init_iface(&state))) {
+    goto ret;
+  }
+
+  if (RACT(!tvpn_server_tcp_init_pipe(state.pipe_fd))) {
+    goto ret;
+  }
+
+  if (RACT(!tvpn_server_tcp_init_socket(&state))) {
+    goto ret;
+  }
+
+ret:
+  tvpn_server_tcp_clean_up(&state);
+  return ret;
 }
