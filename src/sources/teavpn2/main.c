3026cc3acdb3a26bd347e838cbfd8e6e312cf054
Ammar Faizi <ammarfaizi2@gmail.com>
2021-01-26 01:24:45 +0700

    [global] Clean up main function
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/sources/teavpn2/main.c
+++ src/sources/teavpn2/main.c
@@ -6,44 +6,34 @@
 #include <teavpn2/client/entry.h>
 #include <teavpn2/global/helpers/arena.h>
 
-static void usage(const char *app);
-
-int main(int argc, char *argv[])
-{
-	int retval;
-	char arena_buffer[4096];
-
-	retval = 1;
-
-	if (argc <= 1) {
-		usage(argv[0]);
-		goto out;
-	}
-
-	memset(arena_buffer, 0, sizeof(arena_buffer));
-	ar_init(arena_buffer, sizeof(arena_buffer));
-
-	if (strncmp(argv[1], "client", 6) == 0) {
-		retval = teavpn_client_entry(argc, argv);
-	} else
-	if (strncmp(argv[1], "server", 6) == 0) {
-		retval = teavpn_server_entry(argc, argv);
-	} else {
-		printf("Invalid argument: \"%s\"\n", argv[1]);
-		usage(argv[0]);
-		goto out;
-	}
-
-
-out:
-	return retval;
-}
-
-
 static void usage(const char *app)
 {
 	printf("Usage: %s [client|server] [options]\n\n", app);
 	printf("See:\n");
 	printf("   %s server --help\n", app);
 	printf("   %s client --help\n", app);
+}
+
+int main(int argc, char *argv[])
+{
+	char arena_buffer[4096];
+
+	if (argc <= 1) {
+		usage(argv[0]);
+		return 1;
+	}
+
+	memset(arena_buffer, 0, sizeof(arena_buffer));
+	ar_init(arena_buffer, sizeof(arena_buffer));
+
+	if (strncmp(argv[1], "client", 6) == 0) {
+		return teavpn_client_entry(argc, argv);
+	} else
+	if (strncmp(argv[1], "server", 6) == 0) {
+		return teavpn_server_entry(argc, argv);
+	}
+
+	printf("Invalid argument: \"%s\"\n", argv[1]);
+	usage(argv[0]);
+	return 1;
 }
