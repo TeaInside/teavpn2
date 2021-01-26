839e65673ff383af8264a7a3e6dfe3211e5279ea
Ammar Faizi <ammarfaizi2@gmail.com>
2021-01-26 16:21:18 +0700

    [global] Add `NOTICE_STATIC_LEVEL` to limit `prl_notice` at compile time
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/include/teavpn2/global/helpers/debug.h
+++ src/include/teavpn2/global/helpers/debug.h
@@ -10,15 +10,17 @@ extern uint8_t __notice_level;
 void __pr_error(const char *fmt, ...);
 void __pr_notice(const char *fmt, ...);
 
+#define NOTICE_STATIC_LEVEL (10u)
 
 #define pr_error  __pr_error
 #define pr_debug  __pr_debug
 #define pr_notice __pr_notice
-#define prl_notice(LEVEL, ...) 			\
-	do {					\
-		if (__notice_level >= LEVEL) {	\
-			pr_notice(__VA_ARGS__);	\
-		}				\
+#define prl_notice(LEVEL, ...) 					\
+	do {							\
+		if ((NOTICE_STATIC_LEVEL >= (LEVEL))		\
+			&& (__notice_level >= (LEVEL))) {	\
+			pr_notice(__VA_ARGS__);			\
+		}						\
 	} while (0)
 
 #endif /* #ifndef __TEAVPN2__GLOBAL__HELPERS__DEBUG_H */
