8f1a2bd046a982f1b1d0498ecee839ec1e603bc4
Ammar Faizi <ammarfaizi2@gmail.com>
2021-02-22 22:39:34 +0700

    [server] Clean up linux modules
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/teavpn2/server/entry.c
+++ src/teavpn2/server/entry.c
@@ -1,6 +1,37 @@
 
+#include <string.h>
+#include <teavpn2/server/argv.h>
+#include <teavpn2/server/entry.h>
+#include <teavpn2/server/config.h>
 
-int teavpn_server_entry()
+#if defined(__linux__)
+# include <teavpn2/server/plat/linux/tcp.h>
+#else
+# error Target environment is not supported at the moment.
+#endif
+
+int teavpn_server_entry(int argc, char *argv[])
 {
-	return 0;
+	struct srv_cfg cfg;
+
+	memset(&cfg, 0, sizeof(cfg));
+
+	if (server_argv_parse(argc, argv, &cfg) < 0)
+		return 1;
+
+	if (server_cfg_parse(&cfg) < 0)
+		return 1;
+
+	switch (cfg.sock.type) {
+	case SOCK_TCP:
+		return teavpn_tcp_server(&cfg);
+
+	case SOCK_UDP:
+		pr_error("UDP socket is not supported at the moment");
+		return -ESOCKTNOSUPPORT;
+
+	default:
+		pr_error("Invalid socket type: %d", cfg.sock.type);
+		return -EINVAL;
+	}
 }
