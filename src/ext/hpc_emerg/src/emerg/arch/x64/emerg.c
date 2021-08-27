TeaInside/teavpn2

commit 153be4be8d46c780c5e3fd1d45f034d689386f8f

emerg x86-64: dump the %rip for debugging info when panic
When the program panic, add more low level information by dumping
the `%rip`.

Dumping strategy:
1) Write the content of (%rip - 42) with 64 bytes in size into a
   temporary file.

2) Use `objdump` bin from host, execute the following command:
```objdump --adjust-vma=%#lx -D -Mx86-64 -b binary -m i386 '%s'```

In case the host doesn't have `objdump` bin, it will do nothing,
`%rip` dump won't happen.

We attached the output example for better code review experience [1].

Link: https://gist.githubusercontent.com/ammarfaizi2/2472d92eb2d878994d60451855da5a20/raw/f60d3889d674713464d023c361eb64794a86f90d/panic_log_example.txt [1]
Cc: Louvian Lyndal <louvianlyndal@gmail.com>
Reviewed-by: Louvian Lyndal <louvianlyndal@gmail.com>
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/ext/hpc_emerg/src/emerg/arch/x64/emerg.c
@@ -77,8 +77,8 @@ static char alt_stack[ALT_STACK_SIZE];
 static int is_valid_addr(void *addr, size_t len)
 {
 	if (unlikely(vfd == -1)) {
-		// vfd = open(emerg_file_tmp, O_CREAT | O_WRONLY, 0700);
-		vfd = memfd_create("emerg_tmp", MFD_CLOEXEC);
+		vfd = open(emerg_file_tmp, O_CREAT | O_WRONLY, 0700);
+		// vfd = memfd_create("emerg_tmp", MFD_CLOEXEC);
 		if (unlikely(vfd < 0))
 			return 0;
 	}
@@ -153,6 +153,16 @@ static void dump_code(char *buf, void *addr)
 	unsigned char *end, *start = (unsigned char *)addr;
 	start -= 42;
 	end = start + 64;
+	if (!ftruncate(vfd, 0)) {
+		if (lseek(vfd, 0, SEEK_SET) < 0) {
+			strcpy(buf, "lseek() error!");
+		}
+		if (write(vfd, start, (size_t)(end - start)) < 0) {
+			strcpy(buf, "Cannot read from RIP!");
+			return;
+		}
+		fdatasync(vfd);
+	}
 	while (start < end) {
 		buf += sprintf(
 			buf,
@@ -222,6 +232,20 @@ static void print_backtrace(uintptr_t rip)
 			fx.rip_next
 		);
 	}
+}
+
+
+static void dump_rip_insn(void *rip)
+{
+	int ret;
+	char cmd[256];
+	snprintf(cmd, sizeof(cmd),
+		 "objdump --adjust-vma=%#lx -D -Mx86-64 -b binary -m i386 '%s'",
+		 (uintptr_t)rip - 42,
+		 emerg_file_tmp);
+	pr_intr("\n  Executing: %s\n", cmd);
+	ret = system(cmd);
+	(void)ret;
 }
 
 
@@ -318,8 +342,10 @@ static int ___emerg_handler(int sig, siginfo_t *si, ucontext_t *ctx)
 	dump_stack((void *)regs[REG_RSP]);
 	print_backtrace((uintptr_t)rip);
 
-	if (likely(is_recoverable))
+	if (likely(is_recoverable)) {
+		dump_rip_insn((void *)regs[REG_RIP]);
 		emerg_recover(regs);
+	}
 
 	if (likely(vfd != -1)) {
 		close(vfd);

commit 8ffa86d948b1c341887bc789083bfac2aac5d413

emerg x86-64: don't continue the %rip dump if lseek fails
Trivial fix on `dump_code` function.

Fixes: 153be4be8d46c780c5e3fd1d45f034d689386f8f ("emerg x86-64: dump the %rip for debugging info when panic")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/ext/hpc_emerg/src/emerg/arch/x64/emerg.c
@@ -156,6 +156,7 @@ static void dump_code(char *buf, void *addr)
 	if (!ftruncate(vfd, 0)) {
 		if (lseek(vfd, 0, SEEK_SET) < 0) {
 			strcpy(buf, "lseek() error!");
+			return;
 		}
 		if (write(vfd, start, (size_t)(end - start)) < 0) {
 			strcpy(buf, "Cannot read from RIP!");