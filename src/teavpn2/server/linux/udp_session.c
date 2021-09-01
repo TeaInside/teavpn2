TeaInside/teavpn2

commit 667576c933d0c2d74e5313f9565be338b418e905

udp_session.c: implement put_udp_session
`put_udp_session` is a function to free the client slot session. It is
the pair of `get_udp_sess`. Internally implemented atomically with
careful locking mechanism.

This function does 3 important things:
1) Push the `cur_sess->idx` to `state->sess_stk`, it's the lookup
   facility which is introduced in commit e37a4d11f2b5582608f223
   ("server: add stack helper for O(1) client session retrieval").

2) Remove the `udp_sess` reference from the session map.

3) Reset client session slot data (such as: `src_addr`, `src_port`,
   `is_connected`, `username`, etc.).

Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/teavpn2/server/linux/udp_session.c
@@ -120,8 +120,82 @@ struct udp_sess *get_udp_sess(struct srv_udp_state *state, uint32_t addr,
 	addr = htonl(addr);
 	WARN_ON(!inet_ntop(AF_INET, &addr, cur_sess->str_addr,
 			   sizeof(cur_sess->str_addr)));
+
+	udp_sess_tv_update(cur_sess);
+	atomic_store(&cur_sess->is_connected, true);
+	atomic_fetch_add(&state->active_sess, 1);
 out:
 	mutex_unlock(&state->sess_stk_lock);
 	errno = err;
+	return ret;
+}
+
+
+static int remove_sess_from_bkt(struct srv_udp_state *state,
+				struct udp_sess *cur_sess)
+	__acquires(&state->sess_map_lock)
+	__releases(&state->sess_map_lock)
+{
+	int ret = 0;
+	struct udp_sess *sess;
+	struct udp_map_bucket *prev = NULL, *cur, *tmp;
+
+	cur = addr_to_bkt(state->sess_map, cur_sess->src_addr);
+	mutex_lock(&state->sess_map_lock);
+	do {
+		sess = cur->sess;
+		if (sess == cur_sess)
+			goto do_remove;
+
+		prev = cur;
+		cur  = cur->next;
+	} while (cur);
+
+	ret = -ENOENT;
+	goto out;
+
+do_remove:
+	cur->sess = NULL;
+	if (prev == NULL) {
+		/*
+		 * WARNING!!!
+		 * It is illegal to `free(cur)` when `prev == NULL`.
+		 */
+		if (cur->next) {
+			tmp = cur->next->next;
+			cur->sess = cur->next->sess;
+			free(cur->next);
+			cur->next = tmp;
+			pr_debug("put case 0");
+		} else {
+			/* Pass! */
+			pr_debug("put case 1");
+		}
+	} else {
+		pr_debug("put case 2");
+		tmp = cur->next;
+		free(cur);
+		prev->next = tmp;
+	}
+out:
+	mutex_unlock(&state->sess_map_lock);
+	if (ret)
+		errno = -ret;
+	return ret;
+}
+
+
+int put_udp_session(struct srv_udp_state *state, struct udp_sess *cur_sess)
+	__acquires(&state->sess_stk_lock)
+	__releases(&state->sess_stk_lock)
+{
+	int ret;
+	mutex_lock(&state->sess_stk_lock);
+	BUG_ON(bt_stack_push(&state->sess_stk, cur_sess->idx) == -1);
+	if (state->sess_map)
+		ret = remove_sess_from_bkt(state, cur_sess);
+	reset_udp_session(cur_sess, cur_sess->idx);
+	mutex_unlock(&state->sess_stk_lock);
+	atomic_fetch_sub(&state->active_sess, 1);
 	return ret;
 }

commit bebbd12169defd2d96183deaee9bf072a3cb28ca

udp_session.c: fix potential read from uninitialized variable
Link: https://github.com/TeaInside/teavpn2/issues/27
Fixes: 667576c933d0c2d74e5313f9565be338b418e905 ("udp_session.c: implement put_udp_session")
Reported-by: Louvian Lyndal <louvianlyndal@gmail.com>
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/teavpn2/server/linux/udp_session.c
@@ -189,7 +189,7 @@ int put_udp_session(struct srv_udp_state *state, struct udp_sess *cur_sess)
 	__acquires(&state->sess_stk_lock)
 	__releases(&state->sess_stk_lock)
 {
-	int ret;
+	int ret = 0;
 	mutex_lock(&state->sess_stk_lock);
 	BUG_ON(bt_stack_push(&state->sess_stk, cur_sess->idx) == -1);
 	if (state->sess_map)