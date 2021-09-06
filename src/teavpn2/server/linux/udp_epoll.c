TeaInside/teavpn2

commit 7292f968d4ba05512c90804327eace7f23757b60

server: don't treat repeated close packet as new connection
When the client sends `TCLI_PKT_CLOSE` more than once, the first
packet will remove the session entry from map.

However, the rest packet will initiate a new session again because the
server thinks that this is a new client (as it is not found in the map).

Fix for this is that, assume close packet as no-op if it is sent more
than once (only the first packet is processed).

Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/teavpn2/server/linux/udp_epoll.c
@@ -571,6 +571,12 @@ static int __handle_event_udp(struct epl_thread *thread,
 }
 
 
+static bool is_close_packet(struct epl_thread *thread)
+{
+	return thread->pkt.cli.type == TCLI_PKT_CLOSE;
+}
+
+
 static int _handle_event_udp(struct epl_thread *thread, struct sockaddr_in *saddr)
 {
 	uint16_t port;
@@ -585,7 +591,12 @@ static int _handle_event_udp(struct epl_thread *thread, struct sockaddr_in *sadd
 		 * It's a new client since we don't find it in
 		 * the session entry.
 		 */
-		int ret = handle_new_client(thread, addr, port, saddr);
+		int ret
+
+		if (is_close_packet(thread))
+			return 0;
+
+		ret = handle_new_client(thread, addr, port, saddr);
 		return (ret == -EAGAIN) ? 0 : ret;
 	}
 

commit 76da7f380e302201ee0ce089c52b8e6264ce37e5

udp_epoll.c: trivial semicolon fix (syntax error)
Fixes: 7292f968d4ba05512c90804327eace7f23757b60 ("server: don't treat repeated close packet as new connection")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/teavpn2/server/linux/udp_epoll.c
@@ -591,7 +591,7 @@ static int _handle_event_udp(struct epl_thread *thread, struct sockaddr_in *sadd
 		 * It's a new client since we don't find it in
 		 * the session entry.
 		 */
-		int ret
+		int ret;
 
 		if (is_close_packet(thread))
 			return 0;