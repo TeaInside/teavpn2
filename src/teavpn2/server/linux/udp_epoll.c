TeaInside/teavpn2

commit 7292f968d4ba05512c90804327eace7f23757b60

server: don't treat repeated close packet as new connection
When the client sends `TCLI_PKT_CLOSE` more than once, the first
packet will remove the session entry from map.

However, the rest packet will initiate a new session again because the
server thinks that this is a new client (as it is not found in the map).

Fix for this is that, assume close packet as no-op if it is sent more
than once (only the first packet is processed).

Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/teavpn2/server/linux/udp_epoll.c
@@ -571,6 +571,12 @@ static int __handle_event_udp(struct epl_thread *thread,
 }
 
 
+static bool is_close_packet(struct epl_thread *thread)
+{
+	return thread->pkt.cli.type == TCLI_PKT_CLOSE;
+}
+
+
 static int _handle_event_udp(struct epl_thread *thread, struct sockaddr_in *saddr)
 {
 	uint16_t port;
@@ -585,7 +591,12 @@ static int _handle_event_udp(struct epl_thread *thread, struct sockaddr_in *sadd
 		 * It's a new client since we don't find it in
 		 * the session entry.
 		 */
-		int ret = handle_new_client(thread, addr, port, saddr);
+		int ret
+
+		if (is_close_packet(thread))
+			return 0;
+
+		ret = handle_new_client(thread, addr, port, saddr);
 		return (ret == -EAGAIN) ? 0 : ret;
 	}
 

commit 58946806b2e849fd8dd70b744930d7f6afb21af2

server: skip init for several packet types
Add more exceptions type.

Don't init new session for these packet types:
```
 type == TCLI_PKT_CLOSE   ||
 type == TCLI_PKT_PING    ||
 type == TCLI_PKT_REQSYNC ||
 type == TCLI_PKT_SYNC
```

Fixes: 7292f968d4ba05512c90804327eace7f23757b60 ("server: don't treat repeated close packet as new connection")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/teavpn2/server/linux/udp_epoll.c
@@ -571,9 +571,15 @@ static int __handle_event_udp(struct epl_thread *thread,
 }
 
 
-static bool is_close_packet(struct epl_thread *thread)
+static bool packet_doesnt_need_session_init(struct epl_thread *thread)
 {
-	return thread->pkt.cli.type == TCLI_PKT_CLOSE;
+	uint8_t type = thread->pkt.cli.type;
+	return (
+		 type == TCLI_PKT_CLOSE   ||
+		 type == TCLI_PKT_PING    ||
+		 type == TCLI_PKT_REQSYNC ||
+		 type == TCLI_PKT_SYNC
+	);
 }
 
 
@@ -593,9 +599,10 @@ static int _handle_event_udp(struct epl_thread *thread, struct sockaddr_in *sadd
 		 */
 		int ret;
 
-		if (is_close_packet(thread))
+		if (packet_doesnt_need_session_init(thread))
 			return 0;
 
+		pr_debug("%hhu %hhu", thread->pkt.cli.type, (uint8_t)TCLI_PKT_CLOSE);
 		ret = handle_new_client(thread, addr, port, saddr);
 		return (ret == -EAGAIN) ? 0 : ret;
 	}