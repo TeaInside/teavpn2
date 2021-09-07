dbe33659099247bb9c87262d2ba361ca46a57178
Ammar Faizi <ammarfaizi2@gmail.com>
2021-09-07 21:43:57 +0700

    server: refactor udp_support
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/teavpn2/server/linux/udp.c
+++ src/teavpn2/server/linux/udp.c
@@ -14,13 +14,15 @@
 static struct srv_udp_state *g_state = NULL;
 
 
-static void interrupt_handler(int sig)
+static void signal_intr_handler(int sig)
 {
 	struct srv_udp_state *state;
 
 	state = g_state;
-	if (unlikely(!state))
-		panic("interrupt_handler is called when g_state is NULL");
+	if (unlikely(!state)) {
+		panic("signal_intr_handler is called when g_state is NULL");
+		__builtin_unreachable();
+	}
 
 	if (state->sig == -1) {
 		state->stop = true;
@@ -30,11 +32,13 @@ static void interrupt_handler(int sig)
 }
 
 
-static int init_tun_fds(struct srv_udp_state *state)
+static int alloc_tun_fds_array(struct srv_udp_state *state)
 {
-	uint8_t i, nn = (uint8_t)state->cfg->sys.thread_num;
-	int *tun_fds = calloc_wrp((size_t)nn, sizeof(*tun_fds));
+	int *tun_fds;
+	uint8_t i, nn;
 
+	nn      = state->cfg->sys.thread_num;
+	tun_fds = calloc_wrp(nn, sizeof(*tun_fds));
 	if (unlikely(!tun_fds))
 		return -errno;
 
@@ -55,12 +59,25 @@ static int select_event_loop(struct srv_udp_state *state)
 		state->evt_loop = EVTL_EPOLL;
 	} else if (!strcmp(evtl, "io_uring") ||
 		   !strcmp(evtl, "io uring") ||
-		   !strcmp(evtl, "iouring") ||
+		   !strcmp(evtl, "iouring")  ||
 		   !strcmp(evtl, "uring")) {
 		state->evt_loop = EVTL_IO_URING;
 	} else {
 		pr_err("Invalid socket event loop: \"%s\"", evtl);
 		return -EINVAL;
+	}
+
+	switch (state->evt_loop) {
+	case EVTL_EPOLL:
+		state->epl_threads = NULL;
+		break;
+	case EVTL_IO_URING:
+		state->iou_threads = NULL;
+		break;
+	case EVTL_NOP:
+	default:
+		panic("Aiee... invalid event loop value (%u)", state->evt_loop);
+		__builtin_unreachable();
 	}
 	return 0;
 }
@@ -71,11 +88,12 @@ static int init_state(struct srv_udp_state *state)
 	int ret;
 
 	prl_notice(2, "Initializing server state...");
-	g_state = state;
-	state->udp_fd = -1;
-	state->sig = -1;
 
-	ret = init_tun_fds(state);
+	g_state       = state;
+	state->udp_fd = -1;
+	state->sig    = -1;
+
+	ret = alloc_tun_fds_array(state);
 	if (unlikely(ret))
 		return ret;
 
@@ -83,29 +101,17 @@ static int init_state(struct srv_udp_state *state)
 	if (unlikely(ret))
 		return ret;
 
-	switch (state->evt_loop) {
-	case EVTL_EPOLL:
-		state->epl_threads = NULL;
-		break;
-	case EVTL_IO_URING:
-		break;
-	case EVTL_NOP:
-	default:
-		panic("Aiee... invalid event loop value (%u)", state->evt_loop);
-		__builtin_unreachable();
-	}
-
-	prl_notice(2, "Setting up interrupt handler...");
-	if (signal(SIGINT, interrupt_handler) == SIG_ERR)
+	prl_notice(2, "Setting up signal interrupt handler...");
+	if (unlikely(signal(SIGINT, signal_intr_handler) == SIG_ERR))
 		goto sig_err;
-	if (signal(SIGTERM, interrupt_handler) == SIG_ERR)
+	if (unlikely(signal(SIGTERM, signal_intr_handler) == SIG_ERR))
 		goto sig_err;
-	if (signal(SIGHUP, interrupt_handler) == SIG_ERR)
+	if (unlikely(signal(SIGHUP, signal_intr_handler) == SIG_ERR))
 		goto sig_err;
-	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
+	if (unlikely(signal(SIGPIPE, SIG_IGN) == SIG_ERR))
 		goto sig_err;
 
-	prl_notice(2, "Client state initialized successfully!");
+	prl_notice(2, "Server state is initialized successfully!");
 	return ret;
 
 sig_err:
@@ -144,7 +150,7 @@ static int socket_setup(int udp_fd, struct srv_udp_state *state)
 	}
 
 
-	y = 1024 * 1024 * 200;
+	y = 1024 * 1024 * 50;
 	ret = setsockopt(udp_fd, SOL_SOCKET, SO_SNDBUFFORCE, py, len);
 	if (unlikely(ret)) {
 		lv = "SOL_SOCKET";
@@ -167,9 +173,11 @@ static int socket_setup(int udp_fd, struct srv_udp_state *state)
 	 */
 	(void)cfg;
 	return ret;
+
+
 out_err:
 	err = errno;
-	pr_err("setsockopt(udp_fd, %s, %s): " PRERF, lv, on, PREAR(err));
+	pr_err("setsockopt(udp_fd, %s, %s, %d): " PRERF, lv, on, y, PREAR(err));
 	return ret;
 }
 
@@ -182,35 +190,38 @@ static int init_socket(struct srv_udp_state *state)
 	struct sockaddr_in addr;
 	struct srv_cfg_sock *sock = &state->cfg->sock;
 
+
 	type = SOCK_DGRAM;
 	if (state->evt_loop != EVTL_IO_URING)
 		type |= SOCK_NONBLOCK;
 
+
 	prl_notice(2, "Initializing UDP socket...");
-	prl_notice(4, "Calling socket(AF_INET, SOCK_DGRAM%s, 0)...",
-		   (type & SOCK_NONBLOCK) ? " | SOCK_NONBLOCK" : "");
 	udp_fd = socket(AF_INET, type, 0);
 	if (unlikely(udp_fd < 0)) {
 		ret = errno;
 		pr_err("socket(AF_INET, SOCK_DGRAM%s, 0): " PRERF,
-		       (type & SOCK_NONBLOCK) ? " | SOCK_NONBLOCK" : "",
+		       ((type & SOCK_NONBLOCK) ? " | SOCK_NONBLOCK" : ""),
 		       PREAR(ret));
 		return -ret;
 	}
 	prl_notice(2, "UDP socket initialized successfully (fd=%d)", udp_fd);
+
 
 	prl_notice(2, "Setting up socket configuration...");
 	ret = socket_setup(udp_fd, state);
 	if (unlikely(ret))
 		goto out_err;
 
-	prl_notice(2, "Binding UDP socket to %s:%u...", sock->bind_addr,
-		   sock->bind_port);
 
 	memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(sock->bind_port);
 	addr.sin_addr.s_addr = inet_addr(sock->bind_addr);
+	prl_notice(2, "Binding UDP socket to %s:%hu", sock->bind_addr,
+		   sock->bind_port);
+
+
 	ret = bind(udp_fd, (struct sockaddr *)&addr, sizeof(addr));
 	if (unlikely(ret < 0)) {
 		ret = errno;
@@ -218,8 +229,11 @@ static int init_socket(struct srv_udp_state *state)
 		goto out_err;
 	}
 
+
 	state->udp_fd = udp_fd;
 	return 0;
+
+
 out_err:
 	close(udp_fd);
 	return -ret;
@@ -228,24 +242,30 @@ out_err:
 
 static int init_iface(struct srv_udp_state *state)
 {
+	uint8_t i, nn;
+	int ret = 0, tun_fd, *tun_fds;
 	const char *dev = state->cfg->iface.dev;
-	int ret = 0, tun_fd, *tun_fds = state->tun_fds;
-	uint8_t i, nn = (uint8_t)state->cfg->sys.thread_num;
 	short flags = IFF_TUN | IFF_NO_PI | IFF_MULTI_QUEUE;
+
 
 	if (unlikely(!dev || !*dev)) {
 		pr_err("iface dev cannot be empty!");
 		return -EINVAL;
 	}
 
+
 	prl_notice(2, "Initializing virtual network interface (%s)...", dev);
+
+
+	tun_fds = state->tun_fds;
+	nn = state->cfg->sys.thread_num;
 	for (i = 0; i < nn; i++) {
 		prl_notice(4, "Initializing tun_fds[%hhu]...", i);
 
 		tun_fd = tun_alloc(dev, flags);
 		if (unlikely(tun_fd < 0)) {
 			pr_err("tun_alloc(\"%s\", %d): " PRERF, dev, flags,
-				PREAR(-tun_fd));
+			       PREAR(-tun_fd));
 			ret = tun_fd;
 			goto err;
 		}
@@ -254,7 +274,7 @@ static int init_iface(struct srv_udp_state *state)
 			ret = fd_set_nonblock(tun_fd);
 			if (unlikely(ret < 0)) {
 				pr_err("fd_set_nonblock(%d): " PRERF, tun_fd,
-					PREAR(-ret));
+				       PREAR(-ret));
 				close(tun_fd);
 				goto err;
 			}
@@ -285,195 +305,30 @@ err:
 static int init_udp_session_array(struct srv_udp_state *state)
 {
 	int ret = 0;
-	struct udp_sess *sess;
-	uint16_t i, len = UDP_SESS_NUM + 1;
+	struct udp_sess *sess_arr;
+	uint16_t i, len = state->cfg->sock.max_conn;
 
 	prl_notice(4, "Initializing UDP session array...");
-	sess = calloc_wrp((size_t)len, sizeof(*sess));
-	if (unlikely(!sess))
+	sess_arr = calloc_wrp((size_t)len, sizeof(*sess_arr));
+	if (unlikely(!sess_arr))
 		return -errno;
 
-	state->sess = sess;
+	state->sess_arr = sess_arr;
 	for (i = 0; i < len; i++)
-		reset_udp_session(&sess[i], i);
+		reset_udp_session(&sess_arr[i], i);
 
 	return ret;
-}
-
-
-static int init_udp_session_map(struct srv_udp_state *state)
-{
-	int ret;
-	size_t len = 0x100u * 0x100u;
-	struct udp_map_bucket (*sess_map)[0x100u];
-
-	prl_notice(4, "Initializing UDP session map...");
-	sess_map = calloc_wrp((size_t)len, sizeof(struct udp_map_bucket));
-	if (unlikely(!sess_map))
-		return -errno;
-
-	ret = mutex_init(&state->sess_map_lock, NULL);
-	if (unlikely(ret))
-		return -ret;
-
-	state->sess_map = sess_map;
-	return ret;
-}
-
-
-static int init_udp_session_stack(struct srv_udp_state *state)
-{
-	int ret;
-	size_t i;
-
-	prl_notice(4, "Initializing UDP session stack...");
-	if (unlikely(!bt_stack_init(&state->sess_stk, UDP_SESS_NUM)))
-		return -errno;
-
-	ret = mutex_init(&state->sess_stk_lock, NULL);
-	if (unlikely(ret))
-		return -ret;
-
-	for (i = UDP_SESS_NUM; i--;)
-		bt_stack_push(&state->sess_stk, (uint16_t)i);
-
-	return 0;
-}
-
-
-static int init_ipv4_map(struct srv_udp_state *state)
-{
-	uint16_t (*ipv4_map)[0x100];
-
-	ipv4_map = calloc_wrp(0x100ul * 0x100ul, sizeof(uint16_t));
-	if (unlikely(!ipv4_map))
-		return -errno;
-
-	state->ipv4_map = ipv4_map;
-	return 0;
-}
-
-
-static int run_server_event_loop(struct srv_udp_state *state)
-{
-	switch (state->evt_loop) {
-	case EVTL_EPOLL:
-		return teavpn2_udp_server_epoll(state);
-	case EVTL_IO_URING:
-		pr_err("run_client_event_loop() with io_uring: " PRERF,
-			PREAR(EOPNOTSUPP));
-		return -EOPNOTSUPP;
-	case EVTL_NOP:
-	default:
-		panic("Aiee... invalid event loop value (%u)", state->evt_loop);
-		__builtin_unreachable();
-	}
-}
-
-
-static void close_tun_fds(struct srv_udp_state *state)
-{
-	uint8_t i, nn;
-	int *tun_fds = state->tun_fds;
-
-	if (!tun_fds)
-		return;
-
-	nn = (uint8_t)state->cfg->sys.thread_num;
-	for (i = 0; i < nn; i++) {
-		if (tun_fds[i] == -1)
-			continue;
-		prl_notice(2, "Closing tun_fds[%hhu] (fd=%d)...", i, tun_fds[i]);
-	}
-	al64_free(tun_fds);
-}
-
-
-static void close_udp_fd(struct srv_udp_state *state)
-{
-	if (state->udp_fd != -1) {
-		prl_notice(2, "Closing udp_fd (fd=%d)...", state->udp_fd);
-		close(state->udp_fd);
-		state->udp_fd = -1;
-	}
-}
-
-
-static void close_udp_sessions(struct srv_udp_state *state)
-{
-	uint16_t i, len = UDP_SESS_NUM + 1;
-	struct udp_map_bucket (*sess_map)[0x100u] = state->sess_map;
-	struct udp_sess	*sess = state->sess;
-
-	if (!sess)
-		goto free_sess_map;
-
-	for (i = 0; i < len; i++) {
-		if (sess[i].src_addr != 0)
-			put_udp_session(state, &sess[i]);
-	}
-
-	al64_free(sess);
-	state->sess = NULL;
-
-free_sess_map:
-	if (!sess_map)
-		return;
-
-	al64_free(sess_map);
-	state->sess_map = NULL;
-}
-
-
-static void destroy_state(struct srv_udp_state *state)
-{
-	if (state->need_remove_iff) {
-		prl_notice(2, "Removing virtual network interface configuration...");
-		teavpn_iface_down(&state->cfg->iface.iff);
-	}
-
-	if (state->threads_wont_exit) {
-		/*
-		 * WARNING!!!
-		 *
-		 * We are exiting, but the subthreads won't exit.
-		 *
-		 * Having memory leak right before exit is acceptable
-		 * rather than potential UAF since the thread may use
-		 * the freed memory if we free it here.
-		 */
-		pr_emerg("Thread(s) won't exit!");
-		return;
-	}
-
-	close_udp_sessions(state);
-	close_tun_fds(state);
-	close_udp_fd(state);
-
-	bt_stack_destroy(&state->sess_stk);
-
-	mutex_lock(&state->sess_stk_lock);
-	mutex_unlock(&state->sess_stk_lock);
-	mutex_destroy(&state->sess_stk_lock);
-
-	mutex_lock(&state->sess_stk_lock);
-	mutex_unlock(&state->sess_stk_lock);
-	mutex_destroy(&state->sess_map_lock);
-
-	al64_free(state->ipv4_map);
-	al64_free(state);
 }
 
 
 int teavpn2_server_udp_run(struct srv_cfg *cfg)
 {
-	int ret = 0;
+	int ret;
 	struct srv_udp_state *state;
 
-	/* This is a large struct, don't use stack. */
 	state = calloc_wrp(1ul, sizeof(*state));
 	if (unlikely(!state))
-		return -ENOMEM;
+		return -errno;
 
 	state->cfg = cfg;
 	ret = init_state(state);
@@ -488,17 +343,6 @@ int teavpn2_server_udp_run(struct srv_cfg *cfg)
 	ret = init_udp_session_array(state);
 	if (unlikely(ret))
 		goto out;
-	ret = init_udp_session_map(state);
-	if (unlikely(ret))
-		goto out;
-	ret = init_udp_session_stack(state);
-	if (unlikely(ret))
-		goto out;
-	ret = init_ipv4_map(state);
-	if (unlikely(ret))
-		goto out;
-	ret = run_server_event_loop(state);
 out:
-	destroy_state(state);
 	return ret;
 }
--- src/teavpn2/server/linux/udp.h
+++ src/teavpn2/server/linux/udp.h
@@ -16,65 +16,65 @@
 #include <teavpn2/packet.h>
 #include <teavpn2/client/common.h>
 
-#define EPLD_DATA_TUN		(1u << 0u)
-#define EPLD_DATA_UDP		(1u << 1u)
-#define EPOLL_EVT_ARR_NUM	(16)
-
-#define UDP_SESS_TIMEOUT	(10)
-
-#define UDP_SESS_NUM		(32u)
-
-#define UDP_SESS_MAX_ERR	(10u)
-
-#define W_IP(CLIENT) 		((CLIENT)->str_addr), ((CLIENT)->src_port)
-#define W_UN(CLIENT) 		((CLIENT)->username)
-#define W_IU(CLIENT) 		W_IP(CLIENT), W_UN(CLIENT), ((CLIENT)->idx)
-#define PRWIU 			"%s:%d (%s) (cli_idx=%hu)"
-
 
 /*
- * Epoll user data struct.
+ * UDP session struct.
+ *
+ * Each client has its own UDP session struct.
  */
-struct epld_struct {
-	int					fd;
-	unsigned				type;
-	uint16_t				idx;
-};
-
-
-struct srv_udp_state;
-
-
-struct epl_thread {
-	_Atomic(bool)				is_online;
-	uint16_t				idx;
-	pthread_t				thread;
-	int					epoll_fd;
-	int					epoll_timeout;
-	struct srv_udp_state			*state;
-	struct epoll_event			events[EPOLL_EVT_ARR_NUM];
-	alignas(64) struct sc_pkt		pkt;
-};
-
-
-struct udp_map_bucket;
-
-
 struct udp_sess {
+	/*
+	 * Private IP address (virtual network interface).
+	 */
 	uint32_t				ipv4_iff;
+
+	/*
+	 * @src_addr is the UDP session source address.
+	 * @src_port is the UDP session source port.
+	 */
 	uint32_t				src_addr;
 	uint16_t				src_port;
+
+	/*
+	 * UDP sessions are stored in the array, this
+	 * @idx contains the index position of each
+	 * instance.
+	 */
 	uint16_t				idx;
+
+	/*
+	 * Error counter.
+	 */
 	uint16_t				err_c;
-	time_t					last_touch;
+
+	/*
+	 * UDP is stateless, we may not know whether the
+	 * client is still online or not, @last_act can
+	 * be used to handle timeout for session closing
+	 * in case we have abnormal session termination.
+	 */
+	time_t					last_act;
+
+	/*
+	 * Big endian src_addr and src_port for sendto() call.
+	 */
 	struct sockaddr_in			addr;
-	char					str_addr[IPV4_L];
+
+	/*
+	 * Session username.
+	 */
 	char					username[0x100];
+
 	bool					is_authenticated;
 	_Atomic(bool)				is_connected;
 };
 
 
+/*
+ * Bucket for session map. We can handle collision with singly linked
+ * list here.
+ */
+struct udp_map_bucket;
 struct udp_map_bucket {
 	struct udp_map_bucket			*next;
 	struct udp_sess				*sess;
@@ -82,113 +82,175 @@ struct udp_map_bucket {
 
 
 struct srv_udp_state {
+	/*
+	 * @stop is false when event loop is supposed to run.
+	 * @stop is true when event loop needs to be stopped.
+	 */
 	volatile bool				stop;
+
+	/*
+	 * @in_emergency will be true in case we run out of
+	 * buffer, or when we are in the similar urgent
+	 * situation that needs more attention.
+	 */
 	volatile bool				in_emergency;
+
+	/*
+	 * When we're exiting, the main thread will wait for
+	 * the subthreads to exit for the given timeout. If
+	 * the subthreads won't exit, @threads_wont_exit is
+	 * set to true. This is an indicator that we are not
+	 * allowed to free() and close() the resources as it
+	 * may lead to UAF bug.
+	 */
 	bool					threads_wont_exit;
+
+	/*
+	 * @need_remove_iff is true when we need to remove
+	 * virtual network interface configuration before
+	 * exit, otherwise it's false.
+	 */
 	bool					need_remove_iff;
+
+
+	/*
+	 * @sig should contain signal after signal interrupt
+	 * handler is called. If the signal interrupt handle
+	 * is never called, the value of @sig should be -1.
+	 */
 	int					sig;
-	int					udp_fd;
+
 	event_loop_t				evt_loop;
-	int					*tun_fds;
+	int					udp_fd;
 	struct srv_cfg				*cfg;
-	_Atomic(uint16_t)			ready_thread;
 
-	_Atomic(uint16_t)			active_sess;
-	struct udp_sess				*sess;
-
+	/*
+	 * Stack to retrieve free UDP session index in O(1)
+	 * time complexity.
+	 */
 	struct bt_stack				sess_stk;
 	struct tmutex				sess_stk_lock;
 
+	/*
+	 * Small hash table for session lookup after recvfrom().
+	 */
 	struct udp_map_bucket			(*sess_map)[0x100];
 	struct tmutex				sess_map_lock;
 
+	/*
+	 * @sess_arr is an array of UDP sessions.
+	 */
+	struct udp_sess				*sess_arr;
+
+	/*
+	 * Number of active sessions in @sess_arr.
+	 */
+	_Atomic(uint16_t)			n_on_sess;
+
+	/*
+	 * @tun_fds is an array of TUN file descriptors.
+	 * Number of TUN file descriptor can be more than
+	 * one because on Linux it's possible to parallelize
+	 * the read/write to TUN fd.
+	 */
+	int					*tun_fds;
+
+	/*
+	 * Map @ipv4_ff to @sess_arr index.
+	 */
 	uint16_t				(*ipv4_map)[0x100];
 
 	union {
+		/*
+		 * For epoll event loop.
+		 */
 		struct {
-			struct epld_struct	*epl_udata;
 			struct epl_thread	*epl_threads;
+		};
+
+
+		/*
+		 * For io_uring event loop.
+		 */
+		struct {
+			struct iou_thread	*iou_threads;
 		};
 	};
 };
 
 
 extern int teavpn2_udp_server_epoll(struct srv_udp_state *state);
-
+extern int teavpn2_udp_server_io_uring(struct srv_udp_state *state);
 extern struct udp_sess *map_find_udp_sess(struct srv_udp_state *state,
 					  uint32_t addr, uint16_t port);
-
 extern struct udp_sess *get_udp_sess(struct srv_udp_state *state, uint32_t addr,
 				     uint16_t port);
-
 extern int put_udp_session(struct srv_udp_state *state,
 			   struct udp_sess *cur_sess);
 
 
-static inline void reset_udp_session(struct udp_sess *sess, uint16_t idx)
+
+static __always_inline void reset_udp_session(struct udp_sess *sess, uint16_t idx)
 {
-	sess->ipv4_iff = 0;
-	sess->src_addr = 0;
-	sess->src_port = 0;
-	sess->idx = idx;
-	sess->err_c = 0;
-	sess->last_touch = 0;
-	sess->is_authenticated = false;
-	atomic_store(&sess->is_connected, false);
-	sess->str_addr[0] = '\0';
+	sess->ipv4_iff = 0u;
+	sess->src_addr = 0u;
+	sess->src_port = 0u;
+	sess->idx      = idx;
+	sess->err_c    = 0u;
+	sess->last_act = 0;
+	memset(&sess->addr, 0, sizeof(sess->addr));
 	sess->username[0] = '_';
 	sess->username[1] = '\0';
-	memset(&sess->addr, 0, sizeof(sess->addr));
+	sess->is_authenticated = false;
+	atomic_store(&sess->is_connected, false);
 }
 
 
-static inline size_t srv_pprep(struct srv_pkt *srv_pkt, uint8_t type,
-			       uint16_t data_len, uint8_t pad_len)
+static __always_inline size_t srv_pprep(struct srv_pkt *srv_pkt, uint8_t type,
+					uint16_t data_len, uint8_t pad_len)
 {
 	srv_pkt->type    = type;
 	srv_pkt->len     = htons(data_len);
 	srv_pkt->pad_len = pad_len;
-	return data_len + PKT_MIN_LEN;
+	return (size_t)(data_len + PKT_MIN_LEN);
 }
 
 
-static inline size_t srv_pprep_handshake_reject(struct srv_pkt *srv_pkt,
-						uint8_t reason,
-						const char *msg)
+static __always_inline size_t srv_pprep_handshake_reject(struct srv_pkt *srv_pkt,
+							 uint8_t reason,
+							 const char *msg)
 {
 	struct pkt_handshake_reject *rej = &srv_pkt->hs_reject;
+	uint16_t data_len = (uint16_t)sizeof(*rej);
 
 	rej->reason = reason;
-	if (!msg) {
+	if (!msg)
 		memset(rej->msg, 0, sizeof(rej->msg));
-	} else {
+	else
 		strncpy2(rej->msg, msg, sizeof(rej->msg));
-		rej->msg[sizeof(rej->msg) - 1] = '\0';
-	}
 
-	return srv_pprep(srv_pkt, TSRV_PKT_HANDSHAKE_REJECT,
-			 (uint16_t)sizeof(*rej), 0);
+	return srv_pprep(srv_pkt, TSRV_PKT_HANDSHAKE_REJECT, data_len, 0);
 }
 
 
-static inline size_t srv_pprep_handshake(struct srv_pkt *srv_pkt)
+static __always_inline size_t srv_pprep_handshake(struct srv_pkt *srv_pkt)
 {
 	struct pkt_handshake *hand = &srv_pkt->handshake;
 	struct teavpn2_version *cur = &hand->cur;
+	uint16_t data_len = (uint16_t)sizeof(*hand);
 
 	memset(hand, 0, sizeof(*hand));
-	cur->ver = VERSION;
-	cur->patch_lvl = PATCHLEVEL;
-	cur->sub_lvl = SUBLEVEL;
-	strncpy2(cur->extra, EXTRAVERSION, sizeof(cur->extra));
-	cur->extra[sizeof(cur->extra) - 1] = '\0';
 
-	return srv_pprep(srv_pkt, TSRV_PKT_HANDSHAKE, (uint16_t)sizeof(*hand),
-			 0);
+	cur->ver       = VERSION;
+	cur->patch_lvl = PATCHLEVEL;
+	cur->sub_lvl   = SUBLEVEL;
+	strncpy2(cur->extra, EXTRAVERSION, sizeof(cur->extra));
+
+	return srv_pprep(srv_pkt, TSRV_PKT_HANDSHAKE, data_len, 0);
 }
 
 
-static inline int get_unix_time(time_t *tm)
+static __always_inline int get_unix_time(time_t *tm)
 {
 	int ret;
 	struct timeval tv;
@@ -203,65 +265,4 @@ static inline int get_unix_time(time_t *tm)
 }
 
 
-static inline int udp_sess_tv_update(struct udp_sess *cur_sess)
-{
-	return get_unix_time(&cur_sess->last_touch);
-}
-
-
-static inline void add_ipv4_route_map(uint16_t (*ipv4_map)[0x100], uint32_t addr,
-				      uint16_t idx)
-{
-	/*
-	 * IPv4 looks like this:
-	 *     AA.BB.CC.DD
-	 *
-	 * DD is the byte0
-	 * CC is the byte1
-	 */
-
-	uint16_t byte0, byte1;
-
-	byte0 = (addr >> 0u) & 0xffu;
-	byte1 = (addr >> 8u) & 0xffu;
-	ipv4_map[byte0][byte1] = idx + 1u;
-}
-
-
-static inline void del_ipv4_route_map(uint16_t (*ipv4_map)[0x100], uint32_t addr)
-{
-	/*
-	 * IPv4 looks like this:
-	 *     AA.BB.CC.DD
-	 *
-	 * DD is the byte0
-	 * CC is the byte1
-	 */
-
-	uint16_t byte0, byte1;
-
-	byte0 = (addr >> 0u) & 0xffu;
-	byte1 = (addr >> 8u) & 0xffu;
-	ipv4_map[byte0][byte1] = 0;
-}
-
-
-static inline int32_t get_route_map(uint16_t (*ipv4_map)[0x100], uint32_t addr)
-{
-	uint16_t ret, byte0, byte1;
-
-	byte0 = (addr >> 0u) & 0xffu;
-	byte1 = (addr >> 8u) & 0xffu;
-	ret   = ipv4_map[byte0][byte1];
-
-	if (ret == 0) {
-		/* Unmapped address. */
-		return -1;
-	}
-
-	return (int32_t)(ret - 1);
-}
-
-
 #endif /* #ifndef TEAVPN2__SERVER__LINUX__UDP_H */
-
