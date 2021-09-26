TeaInside/teavpn2

commit 04926863f2b458dc5f9c7bcea5d09e277283e3c2

allocator: Implement page aligned allocator for hot buffer
Use `mmap()` directly for hot buffer allocations to make them be page
aligned.

The reason for page aligned buffers is to manage the cache-to-cache
transfers of these buffers, by hardware cache coherence mechanisms,
in SMP systems, so that their movement does not implicitly occur when
other variables are moved. This is for performance critical code,
where one expects contention in the access of variables by multiple
cpus (cores). The usual problem one tries to avoid, in this case, is
false sharing.

Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/teavpn2/allocator.h
@@ -8,11 +8,30 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <sys/mman.h>
 #include <teavpn2/compiler_attributes.h>
 
 extern void *al64_calloc(size_t nmemb, size_t size);
 extern __malloc void *al64_malloc(size_t size);
 extern void al64_free(void *user);
 extern void *al64_realloc(void *user, size_t new_size);
+
+static inline void *al4096_malloc_mmap(size_t size)
+{
+	void *ret;
+
+	ret = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS, -1, 0);
+	if (ret == MAP_FAILED)
+		return NULL;
+
+	return ret;
+}
+
+
+static inline void al4096_free_munmap(void *user, size_t size)
+{
+	if (user != NULL)
+		munmap(user, size);
+}
 
 #endif /* #ifndef TEAVPN2__ALLOCATOR_H */

src/teavpn2/allocator.c
@@ -10,6 +10,7 @@
 #include <teavpn2/allocator.h>
 #include <teavpn2/common.h>
 
+
 noinline void *al64_calloc(size_t nmemb, size_t size)
 {
 	void *orig;

src/teavpn2/client/linux/udp.c
@@ -102,7 +102,7 @@ static int init_state(struct cli_udp_state *state)
 	if (unlikely(ret))
 		return ret;
 
-	pkt = calloc_wrp(1u, sizeof(*pkt));
+	pkt = al4096_malloc_mmap(sizeof(*pkt));
 	if (unlikely(!pkt))
 		return -errno;
 
@@ -678,7 +678,7 @@ static void destroy_state(struct cli_udp_state *state)
 
 	close_tun_fds(state);
 	close_udp_fd(state);
-	al64_free(state->pkt);
+	al4096_free_munmap(state->pkt, sizeof(*state->pkt));
 	al64_free(state);
 }
 

src/teavpn2/client/linux/udp_epoll.c
@@ -161,7 +161,7 @@ static int init_epoll_thread_array(struct cli_udp_state *state)
 		if (unlikely(ret))
 			return ret;
 
-		pkt = calloc_wrp(1ul, sizeof(*pkt));
+		pkt = al4096_malloc_mmap(sizeof(*pkt));
 		if (unlikely(!pkt))
 			return -errno;
 
@@ -709,7 +709,7 @@ static void destroy_epoll(struct cli_udp_state *state)
 	if (threads) {
 		close_epoll_fds(threads, nn);
 		for (i = 0; i < nn; i++)
-			al64_free(threads[i].pkt);
+			al4096_free_munmap(threads[i].pkt, sizeof(*threads[i].pkt));
 	}
 	al64_free(threads);
 }

src/teavpn2/server/linux/udp_epoll.c
@@ -188,7 +188,7 @@ static int init_epoll_thread_array(struct srv_udp_state *state)
 		if (unlikely(ret))
 			return ret;
 
-		pkt = calloc_wrp(1ul, sizeof(*pkt));
+		pkt = al4096_malloc_mmap(sizeof(*pkt));
 		if (unlikely(!pkt))
 			return -errno;
 
@@ -1304,7 +1304,7 @@ static void free_pkt_buffer(struct srv_udp_state *state)
 		return;
 
 	for (i = 0; i < nn; i++)
-		al64_free(threads[i].pkt);
+		al4096_free_munmap(threads[i].pkt, sizeof(*threads[i].pkt));
 }
 
 

commit 6986e5c24baf699cf53581371432472f55a4c486

allocator: Fix `EINVAL` on `al4096_malloc_mmap()`
`MAP_ANONYMOUS` requires `MAP_PRIVATE`.

Fixes: 04926863f2b458dc5f9c7bcea5d09e277283e3c2 ("allocator: Implement page aligned allocator for hot buffer")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/teavpn2/allocator.h
@@ -20,7 +20,8 @@ static inline void *al4096_malloc_mmap(size_t size)
 {
 	void *ret;
 
-	ret = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS, -1, 0);
+	ret = mmap(NULL, size, PROT_READ | PROT_WRITE,
+		   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	if (ret == MAP_FAILED)
 		return NULL;
 