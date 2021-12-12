TeaInside/teavpn2

commit 88cb62fa30cad8b581dcd56d99f68c8bb8ebcbd6

.github/workflows: Use aptitude to resolve crazy dependencies
We are tired resolving crazy 32-bit dependencies. After googled I
found a clean solution by using aptitude. aptitude resolves the
dependencies automatically without requiring us to install them
one by one. We've been trying to fix this multiple times but can't
manage to fix it. Now the solution for this is forthcoming, we're
on the right track now.

Thanks to user "blurfocus" from askubuntu forum who answered a
similar problem [1].

Link: https://askubuntu.com/a/413001 [1]
Fixes: ac843fc59aa4c774 (".github/workflows: Fix missing 32-bit dependencies")
Fixes: dc0f3eca60730795 (".github/workflows: Fix missing 32-bit build library")
Fixes: 30abce1d4fcba4c4 (".github/workflows: Add GUI build for GitHub build bot")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>

.github/workflows/build.yml
@@ -31,16 +31,17 @@ jobs:
 
     - name: Update repo
       run: |
-        sudo apt-get update --fix-missing -y;
+        sudo apt-get update -y --fix-missing;
+
+    - name: Install aptitude
+      run: |
+        sudo apt-get install -y aptitude;
 
     - name: Install tools
       run: |
-        sudo apt-get install -y clang gcc g++ make libc6-dev-i386 gcc-multilib \
-        g++-multilib libgtk-3-common libgtk-3-dev libgtk-3-common:i386 \
-        libgtk-3-dev:i386 gir1.2-gtk-3.0:i386 libatk-bridge2.0-dev:i386 \
-        libatk1.0-dev:i386 libcairo2-dev:i386 libgdk-pixbuf2.0-dev:i386 \
-        libglib2.0-dev:i386 libpango1.0-dev:i386 dconf-gsettings-backend:i386 \
-        libgtk-3-0:i386;
+        sudo aptitude install -y clang gcc g++ make libc6-dev-i386 \
+        gcc-multilib g++-multilib libgtk-3-common libgtk-3-dev \
+        libgtk-3-common:i386 libgtk-3-dev:i386;
 
     - name: Show tools version
       run: |

commit f11104882a198e1fe302712febcc3aa743f8317a

.github: Fix GitHub CI
This reverts commits:
  88cb62fa30cad8b581dcd56d99f68c8bb8ebcbd6
  705503797becb2ebe2ade619905e03506dcf0817
  dc0f3eca607307952bef41196ee957d4977d44df
  30abce1d4fcba4c4337e71e3bf7accc16cb7c962
  c467e70f887730fe3e8d67eb00171e71780cd558
  9376648146ed3f9ccd06a3f66a3fef7f81c60eaa

Alviro added GUI support for GitHub bot, but it breaks the whole CI.

Al, if you are going to fix it, we can reinstate. But as no resolution seems to
be forthcoming right now, let's revert the patches. Let make the CI be green and
not break other essential builds.

Fixes: 88cb62fa30cad8b (".github/workflows: Use aptitude to resolve crazy dependencies")
Fixes: 705503797becb2e (".github/workflows: Fix missing 32-bit dependencies")
Fixes: dc0f3eca6073079 (".github/workflows: Fix missing 32-bit build library")
Fixes: 30abce1d4fcba4c (".github/workflows: Add GUI build for GitHub build bot")
Fixes: c467e70f887730f ("build: Fix 32-bit build with `--gui` option")
Fixes: 9376648146ed3f9 (".github/workflows: Fix build bot error")
Cc: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Acked-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

.github/workflows/build.yml
@@ -25,23 +25,9 @@ jobs:
     - name: Checkout source
       uses: actions/checkout@v2
 
-    - name: Add 32-bit repo
-      run: |
-        sudo dpkg --add-architecture i386;
-
-    - name: Update repo
-      run: |
-        sudo apt-get update -y --fix-missing;
-
-    - name: Install aptitude
-      run: |
-        sudo apt-get install -y aptitude;
-
     - name: Install tools
       run: |
-        sudo aptitude install -y clang gcc g++ make libc6-dev-i386 \
-        gcc-multilib g++-multilib libgtk-3-common libgtk-3-dev \
-        libgtk-3-common:i386 libgtk-3-dev:i386;
+        sudo apt-get install clang gcc g++ make libc6-dev-i386 gcc-multilib g++-multilib libgtk-3-common libgtk-3-dev -y;
 
     - name: Show tools version
       run: |
@@ -54,17 +40,10 @@ jobs:
         make -j$(nproc) clean_all;
         ./configure --cc=${{matrix.cc}} --cxx=${{matrix.cxx}} --ld=${{matrix.cc}};
         make -j$(nproc) DEBUG_MODE=1;
-        make -j$(nproc) clean_all;
-        ./configure --cc=${{matrix.cc}} --cxx=${{matrix.cxx}} --ld=${{matrix.cc}} --gui;
-        make -j$(nproc) DEBUG_MODE=1;
 
     - name: Build
       run: |
-        make -j$(nproc) clean_all;
         ./configure --cc=${{matrix.cc}} --cxx=${{matrix.cxx}} --ld=${{matrix.cc}};
-        make -j$(nproc);
-        make -j$(nproc) clean_all;
-        ./configure --cc=${{matrix.cc}} --cxx=${{matrix.cxx}} --ld=${{matrix.cc}} --gui;
         make -j$(nproc);
 
     - name: Build 32-bit (Debug Mode)
@@ -72,15 +51,8 @@ jobs:
         make -j$(nproc) clean_all;
         ./configure --cc=${{matrix.cc}} --cxx=${{matrix.cxx}} --ld=${{matrix.cc}} --32-bit;
         make -j$(nproc) DEBUG_MODE=1;
-        make -j$(nproc) clean_all;
-        ./configure --cc=${{matrix.cc}} --cxx=${{matrix.cxx}} --ld=${{matrix.cc}} --32-bit --gui;
-        make -j$(nproc) DEBUG_MODE=1;
 
     - name: Build 32-bit
       run: |
-        make -j$(nproc) clean_all;
         ./configure --cc=${{matrix.cc}} --cxx=${{matrix.cxx}} --ld=${{matrix.cc}} --32-bit;
-        make -j$(nproc);
-        make -j$(nproc) clean_all;
-        ./configure --cc=${{matrix.cc}} --cxx=${{matrix.cxx}} --ld=${{matrix.cc}} --32-bit --gui;
         make -j$(nproc);

Makefile
@@ -66,13 +66,9 @@ override C_CXX_FLAGS	:= \
 	-DNAME="\"$(NAME)\"" \
 	-include $(BASE_DIR)/config-host.h $(C_CXX_FLAGS)
 
-ifeq ($(PKG_CONFIG),)
-override PKG_CONFIG := pkg-config
-endif
-
 ifeq ($(CONFIG_GUI),y)
-override LIB_LDFLAGS += $(shell $(PKG_CONFIG) --libs gtk+-3.0)
-override C_CXX_FLAGS += $(shell $(PKG_CONFIG) --cflags gtk+-3.0)
+override LIB_LDFLAGS += $(shell pkg-config --libs gtk+-3.0)
+override C_CXX_FLAGS += $(shell pkg-config --cflags gtk+-3.0)
 endif
 
 override C_CXX_FLAGS_DEBUG := $(C_CXX_FLAGS_DEBUG)

configure
@@ -66,9 +66,6 @@ if test "$USE_32_BIT" = "y"; then
   USER_CFLAGS="-m32 ${USER_CFLAGS}";
   USER_CXXFLAGS="-m32 ${USER_CXXFLAGS}";
   USER_LDFLAGS="-m32 ${USER_LDFLAGS}";
-  PKG_CONFIG=i686-linux-gnu-pkg-config
-else
-  PKG_CONFIG=pkg-config
 fi
 
 if test -z "$prefix"; then
@@ -340,8 +337,6 @@ echo "CXX := $cxx" >> $config_host_mak;
 print_config "CXX" "$cxx";
 echo "LD := $ld" >> $config_host_mak;
 print_config "LD" "$ld";
-echo "PKG_CONFIG := $PKG_CONFIG" >> $config_host_mak;
-print_config "PKG_CONFIG" "$PKG_CONFIG";
 
 echo "CFLAGS := $CFLAGS" >> $config_host_mak;
 print_config "USER_CFLAGS" "$USER_CFLAGS";