TeaInside/teavpn2

commit 07fd9bf3612230721b7f2dc35e1eb986ca1d98b8

Makefile: Fix clang `-Wreserved-identifier` warning
Building with clang:
```
  ./configure --cc=clang --cxx=clang++ --ld=clang++
  make -j16
```
Gives a lot of warnings `-Wreserved-identifier`. This is insane and
should not cause any warning. Append `-Wno-reserved-identifier` for
clang build.

Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Link: https://t.me/TeaInside/47194
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

Makefile
@@ -94,6 +94,7 @@ override CLANG_WARN_FLAGS := \
 	-Wno-disabled-macro-expansion \
 	-Wno-language-extension-token \
 	-Wno-used-but-marked-unused \
+	-Wno-reserved-identifier \
 	-Wno-gnu-statement-expression $(CLANG_WARN_FLAGS)
 
 include $(BASE_DIR)/src/build/flags.make

commit cb06e2b007095e9f29dafe4f5ddacd6eb81a0989

Makefile: Remove `-Weverything` for saner build
Commit 07fd9bf361223 tried to fix a new warning that comes in clang
13, but this breaks the build in clang 10. We should really avoid
using `-Weverything` for default build as it causes so many
portability problems.

Cc: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Fixes: 07fd9bf361223 ("Makefile: Fix clang `-Wreserved-identifier` warning")
Acked-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

Makefile
@@ -87,15 +87,7 @@ override GCC_WARN_FLAGS := \
 override CLANG_WARN_FLAGS := \
 	-Wall \
 	-Wextra \
-	-Weverything \
-	-Wno-padded \
-	-Wno-unused-macros \
-	-Wno-covered-switch-default \
-	-Wno-disabled-macro-expansion \
-	-Wno-language-extension-token \
-	-Wno-used-but-marked-unused \
-	-Wno-reserved-identifier \
-	-Wno-gnu-statement-expression $(CLANG_WARN_FLAGS)
+	$(CLANG_WARN_FLAGS)
 
 include $(BASE_DIR)/src/build/flags.make
 include $(BASE_DIR)/src/build/print.make