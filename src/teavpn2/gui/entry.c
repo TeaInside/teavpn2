796c6ede8610812101ae6968780ad4e89caed086
Khaerul Ilham <khaerulilham163@gmail.com>
2022-01-03 08:10:58 +0700

    gui: Remove XInitThreads()
    Apparently we've forgotten to add the `window` object to the
    `application`. (and we don't really need call `XInitThreads()`
    manually).
    
    Signed-off-by: Khaerul Ilham <khaerulilham163@gmail.com>
    [viro: Remove -lX11 from GUI binary link flags. If we don't
           call XInitThreads, we don't need to link the X11 library. ]
    Co-authored-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
    Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
    Link: https://t.me/TeaInside/48259
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- src/teavpn2/gui/entry.c
+++ src/teavpn2/gui/entry.c
@@ -4,7 +4,6 @@
  * Copyright (C) 2021  Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
  */
 
-#include <X11/Xlib.h>
 #include <teavpn2/gui/gui.h>
 
 static void app_activate(GtkApplication *self, void *user_data)
@@ -22,7 +21,6 @@ int gui_entry(int argc, char *argv[])
 		.self = gtk_application_new(GUI_ID, G_APPLICATION_FLAGS_NONE)
 	};
 
-	XInitThreads();
 	gdk_threads_add_timeout_full(G_PRIORITY_HIGH_IDLE, 500,
 				     client_callback_event_loop, &gui, NULL);
 	ret = teavpn2_gui_event_init(&gui);
