89598337d4a2097f51becf4e28054619e02b3425
Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
2022-01-13 04:47:13 +0700

    print: Add print GUI buffer queue functions
    To create the log feature for the GUI, we need to duplicate the
    printing functions' buffer and transport it to the GTK thread. This
    patch adds buffer queue functions to handle the feature.
    
    Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
    Link: https://t.me/TeaInside/48261
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- src/teavpn2/print.c
+++ src/teavpn2/print.c
@@ -11,6 +11,7 @@
 	#include <pthread.h>
 	static pthread_mutex_t get_time_lock = PTHREAD_MUTEX_INITIALIZER;
 	static pthread_mutex_t print_lock    = PTHREAD_MUTEX_INITIALIZER;
+	static __maybe_unused pthread_mutex_t gui_buf_mutex = PTHREAD_MUTEX_INITIALIZER;
 #else
 	#define pthread_mutex_lock(MUTEX)
 	#define pthread_mutex_unlock(MUTEX)
@@ -18,6 +19,72 @@
 #endif
 
 uint8_t __notice_level = DEFAULT_NOTICE_LEVEL;
+
+
+#ifdef CONFIG_GUI
+
+static char *g_gui_buf = NULL;
+static size_t g_gui_buf_len = 0;
+static size_t g_gui_buf_maxlen = 0;
+
+int gui_pr_buffer_init(size_t len)
+{
+	pthread_mutex_lock(&gui_buf_mutex);
+	g_gui_buf = al64_malloc(len);
+	if (!g_gui_buf) {
+		pthread_mutex_unlock(&gui_buf_mutex);
+		return -ENOMEM;
+	}
+
+	g_gui_buf_len = 0;
+	g_gui_buf_maxlen = len;
+	pthread_mutex_unlock(&gui_buf_mutex);
+	return 0;
+}
+
+size_t gui_pr_consume_buffer(char *buffer, size_t maxlen)
+{
+	size_t cpylen;
+
+	if (unlikely(!maxlen))
+		return 0;
+
+	pthread_mutex_lock(&gui_buf_mutex);
+	if (maxlen < g_gui_buf_len) {
+		size_t unconsumed_pos = maxlen;
+		size_t memmove_len = g_gui_buf_len - unconsumed_pos;
+
+		cpylen = maxlen;
+		memcpy(buffer, g_gui_buf, cpylen);
+		memmove(g_gui_buf, &g_gui_buf[unconsumed_pos], memmove_len);
+		g_gui_buf_len -= cpylen;
+	} else {
+		cpylen = g_gui_buf_len;
+		if (cpylen > 0) {
+			memcpy(buffer, g_gui_buf, cpylen);
+			g_gui_buf_len = 0;
+		}
+	}
+	pthread_mutex_unlock(&gui_buf_mutex);
+	return cpylen;
+}
+
+size_t gui_pr_queue_buffer(const char *buffer, size_t len)
+{
+	size_t cpylen = 0;
+	size_t remaining_size;
+
+	pthread_mutex_lock(&gui_buf_mutex);
+	remaining_size = g_gui_buf_maxlen - g_gui_buf_len;
+	if (likely(remaining_size > 0)) {
+		cpylen = len < remaining_size ? len : remaining_size;
+		memcpy(&g_gui_buf[g_gui_buf_len], buffer, cpylen);
+		g_gui_buf_len += cpylen;
+	}
+	pthread_mutex_unlock(&gui_buf_mutex);
+	return cpylen;
+}
+#endif /* #ifdef CONFIG_GUI */
 
 static __always_inline char *get_time(char *buf)
 	__must_hold(&print_lock)
@@ -61,6 +128,7 @@ void __##NAME(const char *fmt, ...)					\
 	pthread_mutex_lock(&print_lock);				\
 	PR_COPY_BUF(PR, buf, r, vbuf, ul, fmt, vl);			\
 	r = (int) fwrite(vbuf, sizeof(char), (size_t) r, stdout);	\
+	gui_pr_queue_buffer(vbuf, (size_t) r);				\
 	pthread_mutex_unlock(&print_lock);				\
 	va_end(vl);							\
 	(void) r;							\
