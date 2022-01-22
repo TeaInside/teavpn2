714219450ada747231f37198495a9b2ea3d60e3f
Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
2022-01-22 05:06:31 +0000

    mutex: Refactor mutex helpers
    Refactor mutex helpers, make it simpler. The old code was too verbose
    and harder to read.
    
    Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
    Link: https://lore.teainside.org/timl/20220122050633.920866-4-alviro.iskandar@gmail.com/
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/teavpn2/mutex.h
+++ src/teavpn2/mutex.h
@@ -10,99 +10,78 @@
 #include <pthread.h>
 #include <teavpn2/common.h>
 
-#define MUTEX_LEAK_ASSERT 0
-#define MUTEX_LOCK_ASSERT 0
+#ifndef __MUTEX_LEAK_ASSERT
+#define __MUTEX_LEAK_ASSERT 0
+#endif
+
 
 struct tmutex {
 	pthread_mutex_t			mutex;
-#if MUTEX_LEAK_ASSERT
+
+#if __MUTEX_LEAK_ASSERT
 	union {
 		void			*__leak_assert;
-		uint64_t		need_destroy;
+		uintptr_t		need_destroy;
 	};
 #else
 	bool				need_destroy;
-#endif /* #if MUTEX_LEAK_ASSERT */
+#endif
 };
 
 
-static __always_inline void mutex_init_mark(struct tmutex *m)
-{
-#if MUTEX_LEAK_ASSERT
-	m->__leak_assert = malloc(1);
-	if (unlikely(!m->__leak_assert))
-		panic("Cannot initialize __leak_assert for mutex_init_mark");
-#else
-	m->need_destroy = 1;
-#endif /* #if MUTEX_LEAK_ASSERT */
+#define MUTEX_INITIALIZER			\
+{						\
+	.mutex = PTHREAD_MUTEX_INITIALIZER	\
 }
 
+#define DEFINE_MUTEX(V) struct tmutex V = MUTEX_INITIALIZER
 
-static __cold __always_inline int mutex_init(struct tmutex *m,
-					     const pthread_mutexattr_t *attr)
+
+static __always_inline int mutex_init(struct tmutex *m,
+				      const pthread_mutexattr_t *attr)
 {
 	int ret;
 
-	memset(m, 0, sizeof(*m));
 	ret = pthread_mutex_init(&m->mutex, attr);
 	if (unlikely(ret)) {
-		ret = errno;
 		pr_err("pthread_mutex_init(): " PRERF, PREAR(ret));
 		return -ret;
 	}
-	mutex_init_mark(m);
-	return ret;
-}
 
-
-static __hot __always_inline int mutex_lock(struct tmutex *m)
-{
-	int ret;
-	__asm__ volatile("":"+r"(m)::"memory");
-	ret = pthread_mutex_lock(&m->mutex);
-#if MUTEX_LOCK_ASSERT
-	BUG_ON(ret != 0);
+#if __MUTEX_LEAK_ASSERT
+	m->__leak_assert = malloc(1);
+	BUG_ON(!m->__leak_assert);
+#else
+	m->need_destroy = true;
 #endif
+
 	return ret;
 }
 
-
-static __hot __always_inline int mutex_unlock(struct tmutex *m)
+static __always_inline int mutex_lock(struct tmutex *m)
 {
-	int ret;
-	__asm__ volatile("":"+r"(m)::"memory");
-	ret = pthread_mutex_unlock(&m->mutex);
-#if MUTEX_LOCK_ASSERT
-	BUG_ON(ret != 0);
-#endif
-	return ret;
+	return pthread_mutex_lock(&m->mutex);
 }
 
+static __always_inline int mutex_unlock(struct tmutex *m)
+{
+	return pthread_mutex_unlock(&m->mutex);
+}
 
 static __always_inline int mutex_trylock(struct tmutex *m)
 {
-	__asm__ volatile("":"+r"(m)::"memory");
 	return pthread_mutex_trylock(&m->mutex);
 }
 
-
-static __cold inline int mutex_destroy(struct tmutex *m)
+static __always_inline int mutex_destroy(struct tmutex *m)
 {
-	if (m->need_destroy) {
-		int ret = pthread_mutex_destroy(&m->mutex);
-		if (unlikely(ret)) {
-			pr_err("pthread_mutex_destroy(): " PRERF, PREAR(ret));
-			return -ret;
-		}
+	BUG_ON(!m->need_destroy);
 
-#if MUTEX_LEAK_ASSERT
-		free(m->__leak_assert);
-		m->__leak_assert = NULL;
-#else
-		m->need_destroy = 0;
+#if __MUTEX_LEAK_ASSERT
+	free(m->__leak_assert);
 #endif
-	}
-	return 0;
+
+	return pthread_mutex_destroy(&m->mutex);
 }
 
 #endif /* #ifndef TEAVPN2__MUTEX_H */
