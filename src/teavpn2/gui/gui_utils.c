TeaInside/teavpn2

commit 3a2b86328c2a661fd21cde79507c824b47350a72

gui: Create starting template for GUI support
Start from blank window.

Co-authored-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Signed-off-by: Khaerul Ilham <khaerulilham163@gmail.com>
Link: https://t.me/TeaInside/46970
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/teavpn2/gui/gui_utils.c
@@ -1,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2021  Khaerul Ilham <khaerulilham163@gmail.com>
+ */
+
+#include "gui.h"
+
+void gui_utils_set_callback(GuiCallback dest[], guint size)
+{
+	for (guint i = 0; i < size; i++)
+		g_signal_connect(*dest[i].self, dest[i].signal_name,
+				 G_CALLBACK(dest[i].func), dest[i].user_data);
+}
+
+
+/* Probably we should add option flags instead of sets all of them */
+void gui_utils_set_margins(GtkWidget *dest, gint size)
+{
+	gtk_widget_set_margin_start(dest, size);
+	gtk_widget_set_margin_end(dest, size);
+	gtk_widget_set_margin_top(dest, size);
+	gtk_widget_set_margin_bottom(dest, size);
+}

src/teavpn2/gui/Makefile
@@ -10,7 +10,10 @@
 DEP_DIRS += $(BASE_DEP_DIR)/src/teavpn2/gui
 
 OBJ_TMP_CC := \
-	$(BASE_DIR)/src/teavpn2/gui/entry.o
+	$(BASE_DIR)/src/teavpn2/gui/entry.o \
+	$(BASE_DIR)/src/teavpn2/gui/gui_header.o \
+	$(BASE_DIR)/src/teavpn2/gui/gui_utils.o \
+	$(BASE_DIR)/src/teavpn2/gui/gui_window.o
 
 OBJ_PRE_CC += $(OBJ_TMP_CC)
 

src/teavpn2/gui/entry.c
@@ -1,43 +1,27 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2021  Khaerul Ilham <khaerulilham163@gmail.com>
+ */
 
-#include <gtk/gtk.h>
+#include "gui.h"
 
-static void print_hello(GtkWidget *widget, gpointer data)
+static void app_activate(GtkApplication *self, gpointer user_data)
 {
-	(void) widget;
-	(void) data;
-	g_print("Hello World\n");
+	(void) self;
+	gui_window_create((Gui *) user_data);
 }
 
-static void activate(GtkApplication *app, gpointer user_data)
+
+int gui_entry(int argc, char *argv[])
 {
-	GtkWidget *window;
-	GtkWidget *button;
-	GtkWidget *button_box;
+	int ret;
+	Gui gui = {
+		.self = gtk_application_new(GUI_ID, G_APPLICATION_FLAGS_NONE)
+	};
 
-	(void) user_data;
-	window = gtk_application_window_new(app);
-	gtk_window_set_title(GTK_WINDOW(window), "TeaVPN2");
-	gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+	g_signal_connect(gui.self, "activate", G_CALLBACK(app_activate), &gui);
+	ret = g_application_run(G_APPLICATION(gui.self), argc, argv);
+	g_object_unref(gui.self);
 
-	button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
-	gtk_container_add(GTK_CONTAINER(window), button_box);
-
-	button = gtk_button_new_with_label("Click Me");
-	g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-	gtk_container_add(GTK_CONTAINER(button_box), button);
-
-	gtk_widget_show_all(window);
-}
-
-int gui_entry(int argc, char **argv)
-{
-	GtkApplication *app;
-	int status;
-
-	app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-	status = g_application_run(G_APPLICATION(app), argc, argv);
-	g_object_unref(app);
-
-	return status;
+	return ret;
 }

src/teavpn2/gui/gui.h
@@ -1,0 +1,53 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2021  Khaerul Ilham <khaerulilham163@gmail.com>
+ */
+
+#ifndef TEAVPN2__GUI_H
+#define TEAVPN2__GUI_H
+
+#define GUI_ID           "com.teainside.teavpn2"
+#define GUI_PROGRAM_NAME "TeaVPN2"
+#define GUI_WINDOW_TITLE "TeaVPN2 Client"
+#define GUI_WINDOW_RES   500, 600
+
+#include <gtk/gtk.h>
+
+
+typedef struct {
+	GtkApplication *self;
+	struct {
+		GtkWidget     *self;
+		GtkWidget     *child;
+	} window;
+} Gui;
+
+typedef struct {
+	GtkWidget	**self;
+	const gchar	*signal_name;
+	void		(*func)(GtkWidget *, gpointer);
+	gpointer	user_data;
+} GuiCallback;
+
+
+/* gui_window.c */
+void gui_window_create(Gui *g);
+
+/* gui_header.c */
+void gui_header_create(GtkWidget *parent);
+GtkWidget *gui_header_get_button_open(void);
+GtkWidget *gui_header_get_button_about(void);
+
+/* gui_home.c */
+void gui_home_create(GtkWidget *parent);
+GtkWidget *gui_home_get_label_path(void);
+GtkWidget *gui_home_get_button_connect(void);
+GtkWidget *gui_home_get_text_logger(void);
+GtkWidget *gui_home_get_label_status(void);
+
+/* gui_utils.h */
+void gui_utils_set_callback(GuiCallback dest[], guint size);
+void gui_utils_set_margins(GtkWidget *dest, gint size);
+
+
+#endif /* TEAVPN2__GUI_H */

src/teavpn2/gui/gui_header.c
@@ -1,0 +1,76 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2021  Khaerul Ilham <khaerulilham163@gmail.com>
+ */
+
+#include "gui.h"
+
+/* Private functions */
+/* Callbacks */
+static void _button_open_callback (GtkWidget *self, gpointer user_data);
+static void _button_about_callback(GtkWidget *self, gpointer user_data);
+
+
+/* Global (static) variables */
+static GtkWidget *s_w_button_open;
+static GtkWidget *s_w_button_about;
+
+
+/* Public functions */
+void gui_header_create(GtkWidget *parent)
+{
+	GtkWidget   *w_header;
+	GuiCallback  callbacks[] = {
+		{ &s_w_button_open , "clicked", _button_open_callback , NULL },
+		{ &s_w_button_about, "clicked", _button_about_callback, NULL },
+	};
+
+
+	w_header         = gtk_header_bar_new();
+	s_w_button_open  = gtk_button_new_from_icon_name("document-open",
+							 GTK_ICON_SIZE_MENU);
+	s_w_button_about = gtk_button_new_from_icon_name("help-about",
+							 GTK_ICON_SIZE_MENU);
+
+	gui_utils_set_callback(callbacks, G_N_ELEMENTS(callbacks));
+
+	gtk_header_bar_set_show_close_button(GTK_HEADER_BAR(w_header), TRUE);
+	gtk_header_bar_pack_start(GTK_HEADER_BAR(w_header), s_w_button_open);
+	gtk_header_bar_pack_end(GTK_HEADER_BAR(w_header), s_w_button_about);
+
+	gtk_window_set_titlebar(GTK_WINDOW(parent), w_header);
+}
+
+
+GtkWidget *gui_header_get_button_open(void)
+{
+	return s_w_button_open;
+}
+
+
+GtkWidget *gui_header_get_button_about(void)
+{
+	return s_w_button_about;
+}
+
+
+/* Private functions */
+/* Callbacks */
+static void _button_open_callback(GtkWidget *self,
+				  gpointer   user_data)
+{
+	(void)self;
+	(void)user_data;
+
+	g_print("Open\n");
+}
+
+
+static void _button_about_callback(GtkWidget *self,
+				   gpointer   user_data)
+{
+	(void)self;
+	(void)user_data;
+
+	g_print("About\n");
+}

src/teavpn2/gui/gui_window.c
@@ -1,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2021  Khaerul Ilham <khaerulilham163@gmail.com>
+ */
+
+#include "gui.h"
+
+typedef struct {
+	GtkWidget *self;
+	GtkWidget *label;
+} _Notebook;
+
+
+void gui_window_create(Gui *g)
+{
+	g->window.self  = gtk_application_window_new(g->self);
+	g->window.child = gtk_notebook_new();
+
+	gui_header_create(g->window.self);
+	gtk_window_set_title(GTK_WINDOW(g->window.self), GUI_WINDOW_TITLE);
+	gtk_window_set_default_size(GTK_WINDOW(g->window.self), GUI_WINDOW_RES);
+
+	gtk_window_set_resizable(GTK_WINDOW(g->window.self), FALSE);
+	gtk_container_add(GTK_CONTAINER(g->window.self), g->window.child);
+	gtk_widget_show_all(g->window.self);
+}

commit 1970421b37fb8795e134674cc3acf09daf6008b2

gui: Fix wrong logic in gui_utils_set_callback
Don't use "do while" for setting up the callbacks.

If the size happens to be zero, the number of loop will be ((size_t)-1)
which is definitely wrong and will fault.

Fixes: 3a2b86328c2a661fd21cde79507c824b47350a72 ("gui: Create starting template for GUI support")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Reviewed-by: Michael Arminto <kohaku.mski@gmail.com>
Link: https://t.me/TeaInside/48665
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/teavpn2/gui/gui_utils.c
@@ -8,10 +8,7 @@
 
 void gui_utils_set_callback(const struct gui_callback *dest, size_t size)
 {
-	do {
-		size--;
+	while (size--)
 		g_signal_connect(*dest[size].instance, dest[size].signal,
 				 dest[size].func, dest[size].data);
-	} while (size > 0);
 }
-