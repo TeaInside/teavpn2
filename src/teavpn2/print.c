TeaInside/teavpn2

commit 7b781926b54fb2e4e64dfa7fd0efd76777962673

panic.c: don't alter `__emerg_release_bug` on non x86-64 machine
This commit fixes this:
```
   CC		 src/teavpn2/print.o
   LD		 teavpn2
/usr/bin/ld: /home/ammarfaizi/teavpn2/src/teavpn2/print.o: in function `__panic':
/home/ammarfaizi/teavpn2/src/teavpn2/print.c:138: undefined reference to `__emerg_release_bug'
collect2: error: ld returned 1 exit status
make: *** [Makefile:194: teavpn2] Error 1
```

Link: https://t.me/TeaInside/43950
Tested-by: Faiz Jazadi <me@lcat.dev>
Reported-by: Faiz Jazadi <me@lcat.dev>
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/teavpn2/print.c
@@ -119,7 +119,9 @@ void __attribute__((format(printf, 3, 4)))
 __panic(const char *file, int lineno, const char *fmt, ...)
 {
 	va_list vl;
+#if defined(__x86_64__)
 	__emerg_release_bug = true;
+#endif
 	pthread_mutex_trylock(&print_lock);
 	pthread_mutex_trylock(&get_time_lock);
 	puts("=======================================================");

commit 29640924bda00e17a2922cc8716fa41a7da9bd9d

print: Fix the wrong HPC emergency macro checking condition
Nugra reports build error on x86-64 when `CONFIG_HPC_EMERGENCY`
is disabled.

In commit 7b781926b54fb2 ("panic.c: don't alter `__emerg_release_bug`
on non x86-64 machine"), I wrote:
```
  #if defined(__x86_64__)
          __emerg_release_bug = true;
  #endif
```
This is wrong, because if we disable `CONFIG_HPC_EMERGENCY` on x86-64,
we should not alter @__emerg_release_bug variable.

Change it to `#if defined(CONFIG_HPC_EMERGENCY)`.

Fixes: 7b781926b54fb2e4e64dfa7fd0efd76777962673 ("panic.c: don't alter `__emerg_release_bug` on non x86-64 machine")
Link: https://t.me/GNUWeeb/530028
Reported-by: Nugra <richiisei@gmail.com>
Tested-by: Nugra <richiisei@gmail.com>
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/teavpn2/print.c
@@ -145,7 +145,7 @@ DEFINE_PR_FUNC(pr_warn, "Warning: ");
 void __panic(const char *file, int lineno, const char *fmt, ...)
 {
 	va_list vl;
-#if defined(__x86_64__)
+#if defined(CONFIG_HPC_EMERGENCY)
 	__emerg_release_bug = true;
 #endif
 	pthread_mutex_trylock(&print_lock);