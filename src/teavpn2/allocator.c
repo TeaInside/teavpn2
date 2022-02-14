2cdd164dae82d12fe602c227b22b4c0090939928
Arthur Lapz <rlapz@gnuweeb.org>
2022-02-14 10:31:50 +0700

    allocator: Minimize code repetition
    I felt uneasy if I see code repetition :3
    
    Signed-off-by: Arthur Lapz <rlapz@gnuweeb.org>
    Reviewed-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
    Link: https://t.me/GNUWeeb/530404
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- src/teavpn2/allocator.c
+++ src/teavpn2/allocator.c
@@ -11,49 +11,48 @@
 #include <teavpn2/common.h>
 
 
+static __always_inline void *mem_align(void *src, size_t size)
+{
+	void *ret;
+	uint8_t shift;
+
+	ret = (void *)(((uintptr_t)src + size) & ~(size - 1ul));
+	shift = (uint8_t)((uintptr_t)ret - (uintptr_t)src);
+	memcpy((void *)((uintptr_t)ret - 1ul), &shift, 1ul);
+
+	assert(((uintptr_t)ret % size) == 0);
+
+	return ret;
+}
+
+
 noinline void *al64_calloc(size_t nmemb, size_t size)
 {
 	void *orig;
-	void *aligned;
-	uint8_t shift;
 	size_t real_size = 0;
-	size_t extra_size = 63ul + sizeof(shift);
 
 	if (unlikely(__builtin_mul_overflow(nmemb, size, &real_size))) {
 		errno = EOVERFLOW;
 		return NULL;
 	}
 
-	orig = calloc(1u, real_size + extra_size);
+	orig = calloc(1u, real_size + 64ul);
 	if (unlikely(!orig))
 		return NULL;
 
-	aligned = (void *)(((uintptr_t)orig + extra_size) & ~63ul);
-	shift   = (uint8_t)((uintptr_t)aligned - (uintptr_t)orig);
-	memcpy((void *)((uintptr_t)aligned - 1ul), &shift, 1ul);
-
-	assert(((uintptr_t)aligned % 64) == 0);
-	return aligned;
+	return mem_align(orig, 64ul);
 }
 
 
 noinline void *al64_malloc(size_t size)
 {
 	void *orig;
-	void *aligned;
-	uint8_t shift;
-	size_t extra_size = 63ul + sizeof(shift);
 
-	orig = malloc(size + extra_size);
+	orig = malloc(size + 64ul);
 	if (unlikely(!orig))
 		return NULL;
 
-	aligned = (void *)(((uintptr_t)orig + extra_size) & ~63ul);
-	shift   = (uint8_t)((uintptr_t)aligned - (uintptr_t)orig);
-	memcpy((void *)((uintptr_t)aligned - 1ul), &shift, 1ul);
-
-	assert(((uintptr_t)aligned % 64) == 0);
-	return aligned;
+	return mem_align(orig, 64ul);
 }
 
 
@@ -75,25 +74,17 @@ noinline void *al64_realloc(void *user, size_t new_size)
 {
 	void *tmp;
 	void *orig;
-	void *aligned;
 	uint8_t shift;
-	size_t extra_size = 63ul + sizeof(shift);
 
 	if (unlikely(!user))
 		return al64_malloc(new_size);
 
-	memcpy(&shift, (void *)((uintptr_t)user - 1ul), sizeof(shift));
+	memcpy(&shift, (void *)((uintptr_t)user - 1ul), 1ul);
 	orig = (void *)((uintptr_t)user - (uintptr_t)shift);
 
-	tmp = realloc(orig, new_size + extra_size);
+	tmp = realloc(orig, new_size + 64ul);
 	if (unlikely(!tmp))
 		return NULL;
 
-	orig    = tmp;
-	aligned = (void *)(((uintptr_t)orig + extra_size) & ~63ul);
-	shift   = (uint8_t)((uintptr_t)aligned - (uintptr_t)orig);
-	memcpy((void *)((uintptr_t)aligned - 1ul), &shift, 1ul);
-
-	assert(((uintptr_t)aligned % 64) == 0);
-	return aligned;
+	return mem_align(tmp, 64ul);
 }
