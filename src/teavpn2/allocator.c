TeaInside/teavpn2

commit 44f2ca85b3b923b9405b9da89a1e76039b7bafa1

allocator: implement custom allocator to guarantee 64-byte alignment
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/teavpn2/allocator.c
@@ -1,0 +1,98 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ *  Copyright (C) 2021  Ammar Faizi
+ */
+
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <teavpn2/allocator.h>
+#include <teavpn2/common.h>
+
+__no_inline void *al64_calloc(size_t nmemb, size_t size)
+{
+	void *orig;
+	void *aligned;
+	uint8_t shift;
+	size_t real_size = 0;
+	size_t extra_size = 63ul + sizeof(shift);
+
+	if (unlikely(__builtin_mul_overflow(nmemb, size, &real_size))) {
+		errno = EOVERFLOW;
+		return NULL;
+	}
+
+	orig = calloc(1u, real_size + extra_size);
+	if (unlikely(!orig))
+		return NULL;
+
+	aligned = (void *)(((uintptr_t)orig + extra_size) & ~63ul);
+	shift   = (uint8_t)((uintptr_t)aligned - (uintptr_t)orig);
+	memcpy((void *)((uintptr_t)aligned - 1ul), &shift, 1ul);
+
+	assert(((uintptr_t)aligned % 64) == 0);
+	return aligned;
+}
+
+
+__no_inline void *al64_malloc(size_t size)
+{
+	void *orig;
+	void *aligned;
+	uint8_t shift;
+	size_t extra_size = 63ul + sizeof(shift);
+
+	orig = malloc(size + extra_size);
+	if (unlikely(!orig))
+		return NULL;
+
+	aligned = (void *)(((uintptr_t)orig + extra_size) & ~63ul);
+	shift   = (uint8_t)((uintptr_t)aligned - (uintptr_t)orig);
+	memcpy((void *)((uintptr_t)aligned - 1ul), &shift, 1ul);
+
+	assert(((uintptr_t)aligned % 64) == 0);
+	return aligned;
+}
+
+
+__no_inline void al64_free(void *user)
+{
+	void *orig;
+	uint8_t shift;
+
+	if (unlikely(!user))
+		return;
+
+	memcpy(&shift, (void *)((uintptr_t)user - 1ul), 1ul);
+	orig = (void *)((uintptr_t)user - (uintptr_t)shift);
+	free(orig);
+}
+
+
+__no_inline void *al64_realloc(void *user, size_t new_size)
+{
+	void *tmp;
+	void *orig;
+	void *aligned;
+	uint8_t shift;
+	size_t extra_size = 63ul + sizeof(shift);
+
+	if (unlikely(!user))
+		return al64_malloc(new_size);
+
+	memcpy(&shift, (void *)((uintptr_t)user - 1ull), sizeof(shift));
+	orig = (void *)((uintptr_t)user - (uintptr_t)shift);
+
+	tmp = realloc(orig, new_size + extra_size);
+	if (unlikely(!tmp))
+		return NULL;
+
+	orig    = tmp;
+	aligned = (void *)(((uintptr_t)orig + extra_size) & ~63ul);
+	shift   = (uint8_t)((uintptr_t)aligned - (uintptr_t)orig);
+	memcpy((void *)((uintptr_t)aligned - 1ul), &shift, 1ul);
+
+	assert(((uintptr_t)aligned % 64) == 0);
+	return aligned;
+}

src/teavpn2/Makefile
@@ -13,6 +13,7 @@ include $(BASE_DIR)/src/teavpn2/client/Makefile
 include $(BASE_DIR)/src/teavpn2/server/Makefile
 
 OBJ_TMP_CC := \
+	$(BASE_DIR)/src/teavpn2/allocator.o \
 	$(BASE_DIR)/src/teavpn2/main.o \
 	$(BASE_DIR)/src/teavpn2/print.o
 

src/teavpn2/allocator.h
@@ -1,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2021  Ammar Faizi
+ */
+
+#ifndef TEAVPN2__ALLOCATOR_H
+#define TEAVPN2__ALLOCATOR_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+extern void *al64_calloc(size_t nmemb, size_t size);
+extern void *al64_malloc(size_t size);
+extern void al64_free(void *user);
+extern void *al64_realloc(void *user, size_t new_size);
+
+#endif /* #ifndef TEAVPN2__ALLOCATOR_H */

src/teavpn2/common.h
@@ -17,6 +17,7 @@
 #include <inttypes.h>
 
 #include <teavpn2/print.h>
+#include <teavpn2/allocator.h>
 
 #ifndef unlikely
 #  define unlikely(X) __builtin_expect((bool)(X), 0)
@@ -24,6 +25,35 @@
 
 #ifndef likely
 #  define likely(X) __builtin_expect((bool)(X), 1)
+#endif
+
+#if defined(__clang__)
+#  pragma clang diagnostic push
+#  pragma clang diagnostic ignored "-Wreserved-id-macro"
+#endif
+
+#ifndef __maybe_unused
+#  define __maybe_unused __attribute__((unused))
+#endif
+
+#ifndef __inline
+#  define __inline inline
+#endif
+
+#ifndef __always_inline
+#  define __always_inline __inline __attribute__((always_inline))
+#endif
+
+#ifndef __no_inline
+#  define __no_inline __attribute__((noinline))
+#endif
+
+#ifndef __no_return
+#  define __no_return __attribute__((noreturn))
+#endif
+
+#if defined(__clang__)
+#  pragma clang diagnostic pop
 #endif
 
 #ifndef INET_ADDRSTRLEN
@@ -137,5 +167,14 @@ static_assert(sizeof(struct if_info) == 16 + (IPV4_L * 4) + sizeof(uint16_t),
 #endif  /* #ifdef TEAVPN_IPV6_SUPPORT */
 
 extern void show_version(void);
+
+static inline void *calloc_wrp(size_t nmemb, size_t size)
+{
+	void *ret = al64_calloc(nmemb, size);
+	if (unlikely(!ret)) {
+		pr_err("calloc_wrp: " PRERF, PREAR(errno));
+	}
+	return ret;
+}
 
 #endif

src/teavpn2/print.h
@@ -9,8 +9,9 @@
 #define TEAVPN2__PRINT_H
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <teavpn2/common.h>
+#include <stdint.h>
 
 extern uint8_t __notice_level;
 

commit 3541908d4fe36522b6fbffec6d8455a824c89237

allocator: Fix `@errno` value when overflow
The calloc() function from libc sets the @errno variable to ENOMEM
when overflow, not to EOVERFLOW. Change it to ENOMEM to follow libc
error code.

Fixes: 44f2ca85b3b923b9405b9da89a1e76039b7bafa1 ("allocator: implement custom allocator to guarantee 64-byte alignment")
Cc: Ammar Faizi <ammarfaizi2@gmail.com>
Cc: Louvian Lyndal <louvianlyndal@gmail.com>
Cc: Michael Arminto <kohaku.mski@gmail.com>
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
Link: https://lore.gnuweeb.org/gwml/20220527000227.1253934-2-alviro.iskandar@gnuweeb.org
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/teavpn2/allocator.c
@@ -32,7 +32,7 @@ noinline void *al64_calloc(size_t nmemb, size_t size)
 	size_t real_size = 0;
 
 	if (unlikely(__builtin_mul_overflow(nmemb, size, &real_size))) {
-		errno = EOVERFLOW;
+		errno = ENOMEM;
 		return NULL;
 	}
 