TeaInside/teavpn2

commit ca111cce3c05aed1d3a078c095c0111f3c48484f

arch: Add generic arch syscalls from libc
When we add x64 inline syscalls, we forgot to provide syscalls for
generic architecture. Add it.

Fixes: 059c584f6daad7063d653fdb2c0ef71c9df21295 ("Optimize x64 syscalls")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/teavpn2/arch/generic/linux.h
@@ -1,0 +1,58 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2021  Ammar Faizi
+ */
+
+#ifndef TEAVPN2__ARCH__GENERIC__LINUX_H
+#define TEAVPN2__ARCH__GENERIC__LINUX_H
+
+#include <errno.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/syscall.h>
+
+static inline int __sys_epoll_wait(int epfd, struct epoll_event *events,
+				   int maxevents, int timeout)
+{
+	int ret;
+	ret = epoll_wait(epfd, events, maxevents, timeout);
+	return unlikely(ret < 0) ? -errno : ret;
+}
+
+static inline ssize_t __sys_read(int fd, void *buf, size_t len)
+{
+	ssize_t ret;
+	ret = read(fd, buf, len);
+	return unlikely(ret < 0) ? (ssize_t) -errno : ret;
+}
+
+static inline ssize_t __sys_write(int fd, const void *buf, size_t len)
+{
+	ssize_t ret;
+	ret = write(fd, buf, len);
+	return unlikely(ret < 0) ? (ssize_t) -errno : ret;
+}
+
+static inline ssize_t __sys_recvfrom(int sockfd, void *buf, size_t len,
+				     int flags, struct sockaddr *src_addr,
+				     socklen_t *addrlen)
+{
+	ssize_t ret;
+	ret = recvfrom(sockfd, buf, len, flags, src_addr, addrlen);
+	return unlikely(ret < 0) ? (ssize_t) -errno : ret;
+}
+
+static inline ssize_t __sys_sendto(int sockfd, const void *buf, size_t len,
+				   int flags, const struct sockaddr *dest_addr,
+				   socklen_t addrlen)
+{
+	ssize_t ret;
+	ret = sendto(sockfd, buf, len, flags, dest_addr, addrlen);
+	return unlikely(ret < 0) ? (ssize_t) -errno : ret;
+}
+
+#endif /* #ifndef TEAVPN2__ARCH__GENERIC__LINUX_H */

src/teavpn2/client/linux/udp_epoll.c
@@ -263,7 +263,8 @@ static __hot int handle_tun_data(struct epl_thread *thread)
 	write_ret = __sys_write(tun_fd, srv_pkt->__raw, data_len);
 	pr_debug("[thread=%hu] write(tun_fd=%d) %zd bytes", thread->idx, tun_fd,
 		 write_ret);
-	return (write_ret < 0) ? write_ret : 0;
+
+	return (write_ret < 0) ? (int) write_ret : 0;
 }
 
 
@@ -336,7 +337,7 @@ static __hot int handle_event_tun(struct epl_thread *thread, int tun_fd)
 
 		pr_err("read(tun_fd) (fd=%d): " PRERF, tun_fd,
 		       PREAR((int)-read_ret));
-		return read_ret;
+		return (int) read_ret;
 	}
 
 	pr_debug("[thread=%hu] read(tun_fd=%d) %zd bytes", thread->idx, tun_fd,
@@ -344,7 +345,7 @@ static __hot int handle_event_tun(struct epl_thread *thread, int tun_fd)
 
 	send_len = cli_pprep(cli_pkt, TCLI_PKT_TUN_DATA, (uint16_t)read_ret, 0);
 	send_ret = do_send_to(thread, cli_pkt, send_len);
-	return (send_ret < 0) ? (int)send_ret : 0;
+	return (send_ret < 0) ? (int) send_ret : 0;
 }
 
 

src/teavpn2/common.h
@@ -18,20 +18,6 @@
 
 #include <teavpn2/print.h>
 #include <teavpn2/allocator.h>
-
-#include <teavpn2/arch/x86/linux.h>
-
-#ifdef CONFIG_HPC_EMERGENCY
-#  include <emerg/emerg.h>
-#else
-#  define WARN()
-#  define WARN_ONCE()
-#  define WARN_ON(COND) ({ COND; })
-#  define WARN_ON_ONCE(COND) ({ COND; })
-#  define BUG()
-#  define BUG_ON(COND) ({ COND; })
-#endif
-
 #include <teavpn2/compiler_attributes.h>
 
 #ifndef unlikely
@@ -237,5 +223,21 @@ static inline char *strncpy2(char *__restrict__ dst,
 #  pragma GCC diagnostic pop
 #endif
 
+#if defined(__x86_64__)
+#  include <teavpn2/arch/x86/linux.h>
+#else
+#  include <teavpn2/arch/generic/linux.h>
+#endif
+
+#ifdef CONFIG_HPC_EMERGENCY
+#  include <emerg/emerg.h>
+#else
+#  define WARN()
+#  define WARN_ONCE()
+#  define WARN_ON(COND) ({ COND; })
+#  define WARN_ON_ONCE(COND) ({ COND; })
+#  define BUG()
+#  define BUG_ON(COND) ({ COND; })
+#endif
 
 #endif

src/teavpn2/server/linux/udp_epoll.c
@@ -205,7 +205,6 @@ static __hot ssize_t _send_to_client(struct srv_udp_state *state,
 				     const void *buf, size_t pkt_len,
 				     struct sockaddr *dst_addr)
 {
-	int err;
 	ssize_t send_ret;
 	int udp_fd = state->udp_fd;
 	const socklen_t addr_len = sizeof(struct sockaddr_in);
@@ -213,7 +212,7 @@ static __hot ssize_t _send_to_client(struct srv_udp_state *state,
 	if (unlikely(pkt_len == 0))
 		return 0;
 
-	send_ret = sendto(udp_fd, buf, pkt_len, 0, dst_addr, addr_len);
+	send_ret = __sys_sendto(udp_fd, buf, pkt_len, 0, dst_addr, addr_len);
 	if (unlikely(send_ret <= 0)) {
 
 		if (send_ret == 0) {
@@ -221,11 +220,8 @@ static __hot ssize_t _send_to_client(struct srv_udp_state *state,
 			return -ENETDOWN;
 		}
 
-		err = errno;
-		if (err != EAGAIN)
-			pr_err("sendto(): " PRERF, PREAR(err));
-
-		return -err;
+		if (send_ret != -EAGAIN)
+			pr_err("sendto(): " PRERF, PREAR((int)-send_ret));
 	}
 
 	return send_ret;
@@ -892,12 +888,13 @@ static __hot int handle_event_from_tun(struct epl_thread *thread, int tun_fd)
 	read_ret = __sys_read(tun_fd, buf, read_size);
 	if (unlikely(read_ret < 0)) {
 
-		if (likely(read_ret == -EAGAIN))
+		if (read_ret == -EAGAIN)
 			return 0;
 
 		pr_err("read(tun_fd) (fd=%d): " PRERF, tun_fd,
 		       PREAR((int)-read_ret));
-		return read_ret;
+
+		return (int) read_ret;
 	}
 
 	thread->pkt->len = (size_t)read_ret;
@@ -1010,7 +1007,7 @@ static __cold void thread_wait(struct epl_thread *thread,
 }
 
 
-static __cold noinline void *_run_event_loop(void *thread_p)
+static __cold noinline void *server_udp_epoll_run_event_loop(void *thread_p)
 {
 	int ret = 0;
 	struct epl_thread *thread;
@@ -1201,7 +1198,7 @@ static __cold int spawn_tun_worker_thread(struct epl_thread *thread)
 	pthread_t *tr = &thread->thread;
 
 	prl_notice(2, "Spawning thread %u...", thread->idx);
-	ret = pthread_create(tr, NULL, _run_event_loop, thread);
+	ret = pthread_create(tr, NULL, server_udp_epoll_run_event_loop, thread);
 	if (unlikely(ret)) {
 		pr_err("pthread_create(): " PRERF, PREAR(ret));
 		return -ret;
@@ -1219,7 +1216,7 @@ static __cold int spawn_tun_worker_thread(struct epl_thread *thread)
 }
 
 
-static int run_event_loop(struct srv_udp_state *state)
+static __cold int run_event_loop(struct srv_udp_state *state)
 {
 	int ret;
 	void *ret_p;
@@ -1243,7 +1240,7 @@ static int run_event_loop(struct srv_udp_state *state)
 			goto out;
 	}
 
-	ret_p = _run_event_loop(&threads[0]);
+	ret_p = server_udp_epoll_run_event_loop(&threads[0]);
 	ret   = (int)((intptr_t)ret_p);
 out:
 	return ret;

commit c157b7221901d0b2f2ede5ab0f01471ecac91fed

arch/linux: syscall: Fix retval checking in libc syscall
The libc syscall wrappers mostly return -1 when they fail, then they
set the error code to the @errno variable. The current code seems to
be doing something wrong. We assume it errors when the return value
is negative. However, not all negative values are meant to be an
error indicator. On Linux, the only reserved error code is within
range [-4095, -1]. That means we still have a potential to get a
negative return value that is not an error.

I understand that most of them work fine here because of the nature
of the syscall itself that won't return a negative value upen
succcessful. But the above assumption about the negative value is
not correct.

Replace the error checking from (ret < 0) to (ret == -1) to reflect
the above fact.

Fixes: ca111cce3c05aed1d3a078c095c0111f3c48484f ("arch: Add generic arch syscalls from libc")
Cc: Ammar Faizi <ammarfaizi2@gmail.com>
Cc: Louvian Lyndal <louvianlyndal@gmail.com>
Cc: Michael Arminto <kohaku.mski@gmail.com>
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
Link: https://lore.gnuweeb.org/gwml/20220527000227.1253934-4-alviro.iskandar@gnuweeb.org
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/teavpn2/arch/generic/linux.h
@@ -20,21 +20,21 @@ static inline int __sys_epoll_wait(int epfd, struct epoll_event *events,
 {
 	int ret;
 	ret = epoll_wait(epfd, events, maxevents, timeout);
-	return unlikely(ret < 0) ? -errno : ret;
+	return unlikely(ret == -1) ? -errno : ret;
 }
 
 static inline ssize_t __sys_read(int fd, void *buf, size_t len)
 {
 	ssize_t ret;
 	ret = read(fd, buf, len);
-	return unlikely(ret < 0) ? (ssize_t) -errno : ret;
+	return unlikely(ret == -1) ? (ssize_t) -errno : ret;
 }
 
 static inline ssize_t __sys_write(int fd, const void *buf, size_t len)
 {
 	ssize_t ret;
 	ret = write(fd, buf, len);
-	return unlikely(ret < 0) ? (ssize_t) -errno : ret;
+	return unlikely(ret == -1) ? (ssize_t) -errno : ret;
 }
 
 static inline ssize_t __sys_recvfrom(int sockfd, void *buf, size_t len,
@@ -43,7 +43,7 @@ static inline ssize_t __sys_recvfrom(int sockfd, void *buf, size_t len,
 {
 	ssize_t ret;
 	ret = recvfrom(sockfd, buf, len, flags, src_addr, addrlen);
-	return unlikely(ret < 0) ? (ssize_t) -errno : ret;
+	return unlikely(ret == -1) ? (ssize_t) -errno : ret;
 }
 
 static inline ssize_t __sys_sendto(int sockfd, const void *buf, size_t len,
@@ -52,14 +52,14 @@ static inline ssize_t __sys_sendto(int sockfd, const void *buf, size_t len,
 {
 	ssize_t ret;
 	ret = sendto(sockfd, buf, len, flags, dest_addr, addrlen);
-	return unlikely(ret < 0) ? (ssize_t) -errno : ret;
+	return unlikely(ret == -1) ? (ssize_t) -errno : ret;
 }
 
 static inline int __sys_close(int fd)
 {
 	int ret;
 	ret = close(fd);
-	return unlikely(ret < 0) ? -errno : ret;
+	return unlikely(ret == -1) ? -errno : ret;
 }
 
 #endif /* #ifndef TEAVPN2__ARCH__GENERIC__LINUX_H */